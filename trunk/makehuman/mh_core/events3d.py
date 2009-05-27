7290b11e7dd6fde3aadd625cd612e9499c391f75
mflerackers <mflerackers@makehuman.org>
2009-05-27 00:23:00 +0000

    Bug fix in mouse events
    git-svn-id: http://makehuman.googlecode.com/svn@137 ea038faa-237f-11de-8bc4-bb72633a7edc

--- trunk/makehuman/mh_core/events3d.py
+++ trunk/makehuman/mh_core/events3d.py
@@ -10,6 +10,8 @@ class MouseEvent(Event):
     self.button = button
     self.x = x
     self.y = y
+    self.dx = dx
+    self.dy = dy
     
   def __repr__(self):
     return "event: %s, %s, %s, %s, %s" %(self.button, self.x, self.y, self.dx, self.dy)
@@ -197,3 +199,22 @@ SDLK_MENU		      = 319
 SDLK_POWER		    = 320
 SDLK_EURO		      = 321
 SDLK_UNDO		      = 322
+
+
+KMOD_LSHIFT       = 0x0001
+KMOD_RSHIFT       = 0x0002
+KMOD_LCTRL        = 0x0040
+KMOD_RCTRL        = 0x0080
+KMOD_LALT         = 0x0100
+KMOD_RALT         = 0x0200
+KMOD_LMETA        = 0x0400
+KMOD_RMETA        = 0x0800
+KMOD_NUM          = 0x1000
+KMOD_CAPS         = 0x2000
+KMOD_MODE         = 0x4000
+KMOD_RESERVED     = 0x8000
+
+KMOD_CTRL         = KMOD_LCTRL | KMOD_RCTRL
+KMOD_SHIFT        = KMOD_LSHIFT | KMOD_RSHIFT
+KMOD_ALT          = KMOD_LALT | KMOD_RALT
+KMOD_META         = KMOD_LMETA | KMOD_RMETA
