58cbb625e88dead9914aac63cc1d70d05a75d0cc
mflerackers <mflerackers@makehuman.org>
2009-08-21 19:27:27 +0000

    Added interpolators to be used for gui animation and/or model animation
    git-svn-id: http://makehuman.googlecode.com/svn@329 ea038faa-237f-11de-8bc4-bb72633a7edc

--- trunk/makehuman/mh_core/animation3d.py
+++ trunk/makehuman/mh_core/animation3d.py
@@ -21,17 +21,70 @@ Abstract
 This module contains classes defined to implement widgets that provide utility functions
 to the graphical user interface.
 
+To know more about the interpolation methods used, see the following references:
+  http://local.wasp.uwa.edu.au/~pbourke/miscellaneous/interpolation/
+  http://en.wikipedia.org/wiki/Cubic_Hermite_spline
+  http://en.wikipedia.org/wiki/Kochanek-Bartels_spline
+  http://www.geometrictools.com/Documentation/KBSplines.pdf
 """
 
 __docformat__ = 'restructuredtext'
 
-import time
+import time, math
 
-def lerp(v1, v2, alpha):
+# Good interpolator when you have two values to interpolate between, but doesn't give fluid animation
+# when more points are involved since it follows straight lines between the points.
+def linearInterpolate(v1, v2, alpha):
     return v1 + alpha * (v2 - v1)
+    
+# When you have more than 2 points two interpolate (for example following a path), this is a better
+# choice than a linear interpolator.
+def cosineInterpolate(v1, v2, alpha):
+    alpha2 = (1 - math.cos(alpha * math.pi)) / 2
+    return v1 + alpha2 * (v2 - v1)
+    
+# Cubic interpolator. Gives better continuity along the spline than the cosine interpolator,
+# however needs 4 points to interpolate.
+def cubicInterpolate(v0, v1, v2, v3, alpha):
+    alpha2 = alpha * alpha
+    a0 = v3 - v2 - v0 + v1
+    a1 = v0 - v1 - a0
+    a2 = v2 - v0
+    a3 = v1
 
-def lerpVector(v1, v2, alpha):
-    return [v1[0] + alpha * (v2[0] - v1[0]), v1[1] + alpha * (v2[1] - v1[1]), v1[2] + alpha * (v2[2] - v1[2])]
+    return a0 * alpha * alpha2 + a1 * alpha2 + a2 * alpha + a3
+   
+# Hermite interpolator. Allows better control of the bends in the spline by providing two
+# parameters to adjust them:
+#   tension: 1 for high tension, 0 for normal tension and -1 for low tension.
+#   bias: 1 for bias towards the next segment, 0 for even bias, -1 for bias towards the previous segment.
+def hermiteInterpolate(v0, v1, v2, v3, alpha, tension, bias):
+    alpha2 = alpha * alpha
+    alpha3 = alpha2 * alpha
+    m0  = (v1 - v0) * (1 + bias) * (1 - tension) / 2
+    m0 += (v2 - v1) * (1 - bias) * (1 - tension) / 2
+    m1  = (v2 - v1) * (1 + bias) * (1 - tension) / 2
+    m1 += (v3 - v2) * (1 - bias) * (1 - tension) / 2
+    a0 =  2 * alpha3 - 3 * alpha2 + 1
+    a1 =      alpha3 - 2 * alpha2 + alpha
+    a2 =      alpha3 -     alpha2
+    a3 = -2 * alpha3 + 3 * alpha2
+
+    return a0 * v1 + a1 * m0 + a2 * m1 + a3 * v2
+    
+# Kochanek Bartels interpolator. Allows even better control of the bends in the spline by providing three
+# parameters to adjust them:
+#   tension: 1 for high tension, 0 for normal tension and -1 for low tension.
+#   continuity: 1 for inverted corners, 0 for normal corners, -1 for box corners.
+#   bias: 1 for bias towards the next segment, 0 for even bias, -1 for bias towards the previous segment.
+def KochanekBartelsInterpolator(v0, v1, v2, v3, alpha, tension, continuity, bias):
+    # TODO
+    pass
+
+# Interpolates a whole vector at once.
+def lerpVector(v1, v2, alpha, interpolator = linearInterpolate):
+    return [interpolator(v1[i], v2[i], alpha) for i in range(len(v1))]
+    #return [v1[0] + alpha * (v2[0] - v1[0]), v1[1] + alpha * (v2[1] - v1[1]), v1[2] + alpha * (v2[2] - v1[2])]
 
 class Action:
     def __init__():
