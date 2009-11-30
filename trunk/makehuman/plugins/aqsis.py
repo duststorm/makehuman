f38fba3c5bd7eae9894db34852c5b2fec1447824
mbastioni <mbastioni@makehuman.org>
2009-11-30 00:05:27 +0000

    Missed onlyci shader build process at startup.
    git-svn-id: http://makehuman.googlecode.com/svn@419 ea038faa-237f-11de-8bc4-bb72633a7edc

--- trunk/makehuman/plugins/aqsis.py
+++ trunk/makehuman/plugins/aqsis.py
@@ -17,6 +17,7 @@ def load(app):
   # Create aqsis shaders
   #subprocess.Popen("aqsl data/shaders/aqsis/lightmap_aqsis.sl -o data/shaders/aqsis/lightmap.slx", shell=True)
   subprocess.Popen("aqsl data/shaders/renderman/skin.sl -o data/shaders/renderman/skin.slx", shell=True)
+  subprocess.Popen("aqsl data/shaders/renderman/onlyci.sl -o data/shaders/renderman/onlyci.slx", shell=True)
   subprocess.Popen("aqsl data/shaders/renderman/lightmap.sl -o data/shaders/renderman/lightmap.slx", shell=True)
   subprocess.Popen("aqsl data/shaders/renderman/hair.sl -o data/shaders/renderman/hair.slx", shell=True)
   subprocess.Popen("aqsl data/shaders/renderman/shadowspot.sl -o data/shaders/renderman/shadowspot.slx", shell=True)
