d5b6a8f33b9a93fc87378839f816464147e8b7b2
wipkirill <wipkirill@makehuman.org>
2009-12-11 12:29:34 +0000

    Placed the backgrounds before the far clipping region
    git-svn-id: http://makehuman.googlecode.com/svn@450 ea038faa-237f-11de-8bc4-bb72633a7edc

--- trunk/makehuman/main.py
+++ trunk/makehuman/main.py
@@ -1,4 +1,4 @@
-""" 
+"""
 The main MakeHuman Python Application file.
 
 **Project Name:**      MakeHuman
@@ -18,30 +18,30 @@ The main MakeHuman Python Application file.
 Abstract
 --------
 
-This is the main MakeHuman Python Application file which participates in the 
-application startup process. It contains functions that respond to events 
+This is the main MakeHuman Python Application file which participates in the
+application startup process. It contains functions that respond to events
 affecting the main GUI toolbar along the top of the screen in all modes to
-support switching between modes. 
+support switching between modes.
 
-When the MakeHuman application is launched the *main* function from the C 
-application file *main.c* runs. This creates an integration layer by 
-dynamically generating a Python module (called 'mh'). That *main* function 
-then either imports this Python *main* module or executes this Python 
-script *main.py* (depending on platform). 
+When the MakeHuman application is launched the *main* function from the C
+application file *main.c* runs. This creates an integration layer by
+dynamically generating a Python module (called 'mh'). That *main* function
+then either imports this Python *main* module or executes this Python
+script *main.py* (depending on platform).
 
-This script displays a splash screen and a progress bar as it loads the 
+This script displays a splash screen and a progress bar as it loads the
 initial 3D humanoid model (the neutral base object) and adds the various
 GUI sections into the scene. It creates the main toolbar that enables the
-user to switch between different GUI modes and defines functions to 
+user to switch between different GUI modes and defines functions to
 perform that switch for all active buttons. Active buttons are connected
 to these functions by being registered to receive events.
- 
-At the end of the initiation process the splash screen is hidden and 
-Modelling mode is activated. The 'startEventLoop' method on the main Scene3D 
-object is invoked to call the OpenGL/SDL C functions that manage the 
-low-level event loop. 
 
-This Python module responds to high-level GUI toolbar events to switch 
+At the end of the initiation process the splash screen is hidden and
+Modelling mode is activated. The 'startEventLoop' method on the main Scene3D
+object is invoked to call the OpenGL/SDL C functions that manage the
+low-level event loop.
+
+This Python module responds to high-level GUI toolbar events to switch
 between different GUI modes, but otherwise events are handled by GUI mode
 specific Python modules.
 
@@ -70,45 +70,46 @@ import guimodelling, guifiles, guirender
 class MHApplication(gui3d.Application):
   def __init__(self):
     gui3d.Application.__init__(self)
-    
+
     modelCamera = mh.Camera()
-    
+
     mh.cameras.append(modelCamera)
-    
+
     guiCamera = mh.Camera()
     guiCamera.fovAngle = 45
     guiCamera.zoom = 10
     guiCamera.projection = 0
     mh.cameras.append(guiCamera)
-    
+
     self.setTheme("default")
-    
-    # Display the initial splash screen and the progress bar during startup 
+
+    # Display the initial splash screen and the progress bar during startup
     self.splash = gui3d.Object(self, "data/3dobjs/splash.obj", self.getThemeResource("images", "splash.png"), position = [0, 0, 0])
     self.progressBar = gui3d.ProgressBar(self, backgroundTexture = self.getThemeResource("images", "progressbar_background.png"),
       barTexture = self.getThemeResource("images", "progressbar.png"))
     self.scene3d.update()
     self.scene3d.redraw(0)
-    
+
     self.progressBar.setProgress(0.2)
-    
+
     gui3d.Object(self, "data/3dobjs/upperbar.obj", self.getThemeResource("images", "upperbar.png"), [0, 0, 9])
-    gui3d.Object(self, "data/3dobjs/backgroundbox.obj", position = [0, 0, -90])
+    gui3d.Object(self, "data/3dobjs/backgroundbox.obj", position = [0, 0, -89.99])
     gui3d.Object(self, "data/3dobjs/lowerbar.obj", self.getThemeResource("images", "lowerbar.png"), [0, 550, 9])
-    
+
     self.progressBar.setProgress(0.3)
-    
+
     self.scene3d.selectedHuman = human.Human(self.scene3d, "data/3dobjs/base.obj")
+    self.scene3d.selectedHuman.setGender(1.0)
     self.scene3d.selectedHuman.setTexture("data/textures/texture.tif")
-    
+
     self.progressBar.setProgress(0.6)
-    
+
     self.tool = None
     self.selectedGroup = None
-    
+
     self.undoStack = []
     self.redoStack = []
-    
+
     @self.scene3d.selectedHuman.event
     def onMouseDown(event):
       if self.tool:
@@ -116,60 +117,60 @@ class MHApplication(gui3d.Application):
         self.tool.callEvent("onMouseDown", event)
       else:
         self.currentTask.callEvent("onMouseDown", event)
-      
+
     @self.scene3d.selectedHuman.event
     def onMouseMoved(event):
       if self.tool:
         self.tool.callEvent("onMouseMoved", event)
       else:
         self.currentTask.callEvent("onMouseMoved", event)
-      
+
     @self.scene3d.selectedHuman.event
     def onMouseDragged(event):
       if self.tool:
         self.tool.callEvent("onMouseDragged", event)
       else:
         self.currentTask.callEvent("onMouseDragged", event)
-      
+
     @self.scene3d.selectedHuman.event
     def onMouseUp(event):
       if self.tool:
         self.tool.callEvent("onMouseUp", event)
       else:
         self.currentTask.callEvent("onMouseUp", event)
-        
+
     @self.scene3d.selectedHuman.event
     def onMouseEntered(event):
       if self.tool:
         self.tool.callEvent("onMouseEntered", event)
       else:
         self.currentTask.callEvent("onMouseEntered", event)
-      
+
     @self.scene3d.selectedHuman.event
     def onMouseExited(event):
       if self.tool:
         self.tool.callEvent("onMouseExited", event)
       else:
         self.currentTask.callEvent("onMouseExited", event)
-      
+
     # Set up categories and tasks
-    
+
     # Exit button
     category = gui3d.Category(self, "Exit", self.getThemeResource("images", "button_exit.png"))
     @category.button.event
     def onClicked(event):
       self.stop()
-    
+
     guimodelling.ModellingCategory(self)
     self.progressBar.setProgress(0.7)
     guifiles.FilesCategory(self)
     self.progressBar.setProgress(0.8)
     guirender.RenderingCategory(self)
-    
+
     library = gui3d.Category(self, "Library", self.getThemeResource("images", "button_library.png"))
     hair.HairTaskView(library)
     background.BackgroundTaskView(library)
-    
+
     # Load plugins not starting with _
     self.modules = {}
     for path in glob.glob(join("plugins/",'[!_]*.py')):
@@ -177,23 +178,23 @@ class MHApplication(gui3d.Application):
         module = imp.load_source(name, path)
         self.modules[name] = module
         module.load(self)
-    
+
     category = gui3d.Category(self, "Help", self.getThemeResource("images", "button_about.png"))
     # Help button
     @category.button.event
     def onClicked(event):
       webbrowser.open(os.getcwd()+"/docs/MH_Users_Guide.pdf");
-    
+
     self.switchCategory("Modelling")
-    
+
     self.progressBar.setProgress(1.0)
     self.progressBar.hide()
-    
+
   def onStart(self, event):
       self.splash.hide()
       self.scene3d.selectedHuman.applyAllTargets(self.app.progress)
       mh.updatePickingBuffer();
-      
+
   def onKeyDown(self, event):
     if event.modifiers & events3d.KMOD_CTRL:
       if event.key == events3d.SDLK_m:
@@ -223,7 +224,7 @@ class MHApplication(gui3d.Application):
           if stereoMode > 2:
             stereoMode = 0
           mh.cameras[0].stereoMode = stereoMode
-          
+
           # We need a black background for stereo
           background = self.app.categories["Modelling"].tasksByName["Macro modelling"].background
           if stereoMode:
@@ -234,22 +235,22 @@ class MHApplication(gui3d.Application):
             for f in g.faces:
               f.color = [color, color, color]
               f.updateColors()
-          
+
           self.app.scene3d.redraw()
-    
+
   def do(self, action):
     if action.do():
       self.undoStack.append(action)
       del self.redoStack[:]
       print("do " + action.name)
       self.scene3d.redraw()
-      
+
   def did(self, action):
     self.undoStack.append(action)
     del self.redoStack[:]
     print("did " + action.name)
     self.scene3d.redraw()
-    
+
   def undo(self):
     if self.undoStack:
       action = self.undoStack.pop()
@@ -257,7 +258,7 @@ class MHApplication(gui3d.Application):
       action.undo()
       self.redoStack.append(action)
       self.scene3d.redraw()
-    
+
   def redo(self):
     if self.redoStack:
       action = self.redoStack.pop()
@@ -265,32 +266,32 @@ class MHApplication(gui3d.Application):
       action.do()
       self.undoStack.append(action)
       self.scene3d.redraw()
-  
+
   def setTheme(self, theme):
     f = open("data/themes/" + theme + ".mht", 'r')
-      
+
     for data in f.readlines():
       lineData = data.split()
-      
+
       if len(lineData) > 0:
         if lineData[0] == "version":
           print("Version " + lineData[1])
         elif lineData[0] == "color":
           if lineData[1] == "clear":
             mh.setClearColor(float(lineData[2]), float(lineData[3]), float(lineData[4]), float(lineData[5]))
-    
+
     self.theme = theme
-  
+
   def getThemeResource(self, folder, id):
     return "data/themes/" + self.theme + "/" + folder + "/"+ id
-    
+
   def progress(self, value):
     self.progressBar.setProgress(value)
     if value <= 0:
       self.progressBar.show()
     elif value >= 1.0:
       self.progressBar.hide()
-    
+
 application = MHApplication()
 mainScene = application.scene3d # HACK: Don't remove this, it is needed to receive events from C
 application.start()
