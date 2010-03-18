de4fb7c15263639e1e9569743efcd70432f187d7
mbastioni <mbastioni@makehuman.org>
2010-03-18 11:12:38 +0000

    Clean up of topology translator code (not finished yet)
    git-svn-id: http://makehuman.googlecode.com/svn@802 ea038faa-237f-11de-8bc4-bb72633a7edc

--- trunk/makehuman/utils/topology_translator/topology_translator.py
+++ trunk/makehuman/utils/topology_translator/topology_translator.py
@@ -1,13 +1,30 @@
+# You may use, modify and redistribute this module under the terms of the GNU GPL3.0.
+"""
+Translate a  morph target from a mesh with topology 1 to a mesh with topology 2
+
+===========================  ==================================================================
+Project Name:                **MakeHuman**
+Module File Location:        utils/topology_translator/topology_translator.py
+Product Home Page:           http://www.makehuman.org/
+Authors:                     Manuel Bastioni
+Copyright(c):                MakeHuman Team 2001-2010
+Licensing:                   GPL3 (see also: http://sites.google.com/site/makehumandocs/licensing)
+Coding Standards:            See http://sites.google.com/site/makehumandocs/developers-guide#TOC-Coding-Style
+===========================  ==================================================================
+
+
+"""
+
 from math import sqrt
 from aljabr import *
-import time
 import simpleoctree
+import sys
 
 
 
-    
-    
-def loadVertsCoo(path, tesselate = None):
+
+
+def loadVertsCoo(path):
     """
     This function serves as a small utility function to load just the vertex
     data from a WaveFront object file.
@@ -31,28 +48,46 @@ def loadVertsCoo(path, tesselate = None):
     faces = []
     for data in fileDescriptor:
         lineData = data.split()
- 
+
         if lineData[0] == 'v':
             co = [float(lineData[1]), float(lineData[2]), float(lineData[3])]
-            verts.append(co)                
+            verts.append(co)
     fileDescriptor.close()
-    
+
     return verts
 
 
 
 def tesselate(path, vertices):
+    """
+    This function make a very simple tesselation, based on verts only.
+
+
+    Parameters
+    ----------
+
+    path:
+        *string*. The wavefront obj to tesselate. It's needed to get the
+        faces informations.
+
+    vertices:
+        *list*. The list of verts coords to be subdivided. They can't be loaded from
+        the wavefront above, because it's supposed the coordinates are
+        changed by previously applied morphs.
+
+    """
+
     try:
         fileDescriptor = open(path)
     except:
         print 'Error opening %s file' % path
         return
-    
+
     faces = []
     for data in fileDescriptor:
-        lineData = data.split()        
+        lineData = data.split()
 
-        if lineData[0] == 'f':           
+        if lineData[0] == 'f':
             face = []
             for faceData in lineData[1:]:
                 vInfo = faceData.split('/')
@@ -97,27 +132,37 @@ def tesselate(path, vertices):
             newVert8 = centroid([vertices[i0],vertices[i1],newVert4])
             newVert9 = centroid([vertices[i1],vertices[i2],newVert4])
             newVert10 = centroid([vertices[i2],vertices[i0],newVert4])
-            subdividedVerts.extend([newVert4,newVert5,newVert6,newVert7,newVert8,newVert9,newVert10])        
-            
+            subdividedVerts.extend([newVert4,newVert5,newVert6,newVert7,newVert8,newVert9,newVert10])
 
 
     finalVertList = vertices + subdividedVerts
-    print "DEBUG"
-    print len(vertices)
-    print len(subdividedVerts)
-    print len (finalVertList)
-    
+    print "ORIGINAL VERTS: %i"%(len(vertices))
+    print "VERTS ADDED BY SUBDIVISION: %i"%(len(subdividedVerts))
     return finalVertList
-    
-
-
-
-
 
 
 
 def loadTranslationTarget(vertsList, targetPath):
+    """
+    This function load and apply, with value 1, a morph target.
 
+
+    Parameters
+    ----------
+
+    vertsList:
+        *list*. The list of verts coords to be modified by the morph.
+
+    path:
+        *string*. The wavefront obj to tesselate. It's needed to get the
+        faces informations.
+
+    vertsList:
+        *list*. The vertices to be subdivided. They can't be loaded from
+        the wavefront above, because it's supposed the coordinates are
+        changed by previously applied morphs.
+
+    """
     try:
         fileDescriptor = open(targetPath)
     except:
@@ -128,12 +173,10 @@ def loadTranslationTarget(vertsList, targetPath):
         translationData = line.split()
         if len(translationData) == 4:
             vertIndex = int(translationData[0])
-            
             # Adding the translation vector
             vertsList[vertIndex][0] += float(translationData[1])
             vertsList[vertIndex][1] += float(translationData[2])
             vertsList[vertIndex][2] += float(translationData[3])
-
     fileDescriptor.close()
     return True
 
@@ -142,102 +185,105 @@ def loadTranslationTarget(vertsList, targetPath):
 
 def saveData(newMeshPath, oldMeshPath, dataPath):
 
+    #We load the old mesh coords, and then tesselate it, in order
+    #to have a better result in linking new mesh.
     vertsList1 = loadVertsCoo(newMeshPath)
     vertsList2 = tesselate(oldMeshPath, loadVertsCoo(oldMeshPath))
-    
+
+    overwrite = 0 #Just for more elegant one-line print output progress
+
     for i,v in enumerate(vertsList2):
         v.append(i)
-    
-    octree = soctree.SimpleOctree(vertsList2, .25)
+
+    octree = simpleoctree.SimpleOctree(vertsList2, .25)
     try:
         fileDescriptor = open(dataPath, 'w')
     except:
         print 'Unable to open %s', dataPath
-        return None    
+        return None
 
-       
+    #For each vert of new mesh we found the nearest verts of old one
     for i1,v1 in enumerate(vertsList1):
-        t1 = time.time() 
-        #For each vert of new mesh...
-              
-        vIndices = []
-        vDistances = [] 
-        
-        
-        vertsList3 = octree.root.getSmallestChild(v1)        
-                 
 
+
+        vIndices = []
+        vDistances = []
+
+        #We use octree to search only on a small part of the whole old mesh.
+        vertsList3 = octree.root.getSmallestChild(v1)
+
+        #This dictionary is needed to have an ordered list of distances
         distData = {}
-        
+
         #... find nearest verts on old mesh
-        for i2,v2 in enumerate(vertsList3.verts):            
+        for i2,v2 in enumerate(vertsList3.verts):
             d = vdist(v1, v2)
             distData[d]=v2[3]
 
+        #The trick to have an ordered list of distances, linked to the indices.
+        #We use distances as keys.
         dKeys = distData.keys()
         dKeys.sort()
-        dmin = dKeys[0]
+        dmin = dKeys[0] #The first one is the smallest.
 
-        #in case of smaller distance < 0.005 the vert is identical
+        #in case of smaller distance < 0.005
+        #new vert and old vert are coincided, so no more verts are needed.
         if  dKeys[0] < 0.005:
             vIndices = [distData[dKeys[0]]]
-            vDistances = [dKeys[0]]           
-            
-        #else we get the 3 first verts with smaller distance              
-        else:                
-            vIndices = [distData[dKeys[0]],\
-                distData[dKeys[1]],\
-                distData[dKeys[2]],distData[dKeys[3]],distData[dKeys[4]],distData[dKeys[5]],distData[dKeys[6]]]
-            vDistances = [dKeys[0],dKeys[1],dKeys[2],dKeys[3],dKeys[4],dKeys[5],dKeys[6]]
-            
-        if d < dmin:
-            dmin = d                 
+            vDistances = [dKeys[0]]
 
+        #else we get the 7 first verts with smaller distance
+        else:
+            if len(dKeys) > 7:
+                vDistances = dKeys[:7]
+            else:
+                vDistances = dKeys[:(len(dKeys)-1)]
+            vIndices = [distData[n] for n in vDistances]
+
+        #Now we have verts indices and distances from the input vert, so
+        #we can calculate their weights
         if dmin > 0:
             weights = [dmin/dst for dst in vDistances]
         else:
-            weights = [1]        
-            
-        if vIndices ==[]:
-            print "error"
-            break
-            
+            weights = [1]
+
+        #Finally we write the data
         for index in vIndices:
             fileDescriptor.write('%i ' % (index))
         for weight in weights:
-            fileDescriptor.write('%f ' % (weight))            
+            fileDescriptor.write('%f ' % (weight))
         fileDescriptor.write('\n')
-        t = time.time()-t1
-        print "Time remaining: %f hours"%((t*(len(vertsList1)-i1))/3600.0)  
-         
-        
+
+
+        word = "Linking verts: %.2f%c."%((float(i1)/len(vertsList1))*100, "%")
+        sys.stdout.write("%s%s\r" % (word, " "*overwrite ))
+        sys.stdout.flush()
+        overwrite = len(word)
+
+
     fileDescriptor.close()
 
 
 
 
 def translateMorph(objOldPath, targetOldPath, objNewPath, oldMeshPath, dataPath):
-    
+
 
     vertsOld1 = loadVertsCoo(objOldPath)
-    vertsNew = loadVertsCoo(objNewPath)    
+    vertsNew = loadVertsCoo(objNewPath)
     loadTranslationTarget(vertsOld1, targetOldPath)
-    vertsOld = tesselate(oldMeshPath, vertsOld1)  
+    vertsOld = tesselate(oldMeshPath, vertsOld1)
 
     try:
         fileDescriptor = open(dataPath)
     except:
         print 'Unable to open %s', dataPath
         return
-    
+
     idx = 0
     for line in fileDescriptor:
         translationData = line.split()
-        if idx == 9030:
-            print "DATA: ",translationData
 
-        
-        #print "DATA: ",translationData
         halfList = len(translationData)/2
         xIdx = translationData[:halfList]
         xWeight = translationData[halfList:]
@@ -249,23 +295,16 @@ def translateMorph(objOldPath, targetOldPath, objNewPath, oldMeshPath, dataPath)
         for i in range(len(xIdx)):
             index = int(xIdx[i])
             weight = float(xWeight[i])
-            
+
             vertOld = vmul(vertsOld[index],weight)
             xSum = vadd(xSum,vertOld)
 
             if idx == 9030:
                 print "weight", weight
                 print "vert old", vertsOld[index]
-                
 
-        vertsNew[idx] = vdiv(xSum,sumWeight)
-        
-        if idx == 9030:
-            print "Vert sum", xSum
-            print "Vert result", vertsNew[idx]
-   
+        vertsNew[idx] = vmul(xSum,1.0/sumWeight)
         idx += 1
-            
     fileDescriptor.close()
     return vertsNew
 
@@ -273,15 +312,15 @@ def translateMorph(objOldPath, targetOldPath, objNewPath, oldMeshPath, dataPath)
 
 
 def saveTranslationTarget(objPath, targetPath, targetVerts, epsilon=0.001):
-    
+
 
     originalVerts = loadVertsCoo(objPath)
     modifiedVertsIndices = []
     nVertsExported = 0
     for i in range(len(targetVerts)):
-        
+
         originalVertex = originalVerts[i]
-        targetVertex = targetVerts[i]        
+        targetVertex = targetVerts[i]
 
         delta = vsub(targetVertex, originalVertex)
         dist = vdist(originalVertex, targetVertex)
@@ -299,7 +338,7 @@ def saveTranslationTarget(objPath, targetPath, targetVerts, epsilon=0.001):
     for data in modifiedVertsIndices:
         fileDescriptor.write('%d %f %f %f\n' % (data[0], data[1], data[2], data[3]))
     fileDescriptor.close()
-    
+
     print 'Exported %d verts '%(nVertsExported)
 
 
@@ -308,9 +347,9 @@ def saveTranslationTarget(objPath, targetPath, targetVerts, epsilon=0.001):
 
 
 
-    
-           
-    
+
+
+
 
 
 
@@ -321,5 +360,5 @@ vertsToSave = translateMorph("baseOld.obj", "test.target", "baseNew.obj","baseOl
 saveTranslationTarget("baseNew.obj", "result.target", vertsToSave, epsilon=0.001)
 
 
-                
-            
+
+
