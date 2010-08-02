2b0a35a06b99a87271828a612c4043a79c6dee25
jocapsco <jocapsco@makehuman.org>
2010-08-02 08:27:07 +0000

    deleting makehuman folder.. sorry guys
    git-svn-id: http://makehuman.googlecode.com/svn@1250 ea038faa-237f-11de-8bc4-bb72633a7edc

--- trunk/makehuman/makehuman/utils/formatter.bat
+++
@@ -1,5 +0,0 @@
-astyle  --style=ansi  ../src/main.c  ../src/core.c ../src/glmodule.c ../include/core.h ../include/glmodule.h
-
-for %%X in (../mh_core/*.py) do pythontidy.py ../mh_core/%%X ../mh_core/%%X
-for %%X in (../mh_plugins/*.py) do pythontidy.py ../mh_plugins/%%X ../mh_plugins/%%X
-for %%X in (../plugins/*.py) do pythontidy.py ../plugins/%%X ../plugins/%%X
