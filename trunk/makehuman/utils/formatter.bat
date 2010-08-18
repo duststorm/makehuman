6660928d168991913122cefd78904dc093862187
jocapsco <jocapsco@makehuman.org>
2010-08-18 19:37:16 +0000

    modified formatter for the new directory names (apps and core)
    git-svn-id: http://makehuman.googlecode.com/svn@1288 ea038faa-237f-11de-8bc4-bb72633a7edc

--- trunk/makehuman/utils/formatter.bat
+++ trunk/makehuman/utils/formatter.bat
@@ -1,5 +1,5 @@
 astyle  --style=ansi  ../src/main.c  ../src/core.c ../src/glmodule.c ../include/core.h ../include/glmodule.h
 
-for %%X in (../mh_core/*.py) do pythontidy.py ../mh_core/%%X ../mh_core/%%X
-for %%X in (../mh_plugins/*.py) do pythontidy.py ../mh_plugins/%%X ../mh_plugins/%%X
+for %%X in (../core/*.py) do pythontidy.py ../core/%%X ../core/%%X
+for %%X in (../apps/*.py) do pythontidy.py ../apps/%%X ../apps/%%X
 for %%X in (../plugins/*.py) do pythontidy.py ../plugins/%%X ../plugins/%%X
