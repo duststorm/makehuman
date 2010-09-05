645e07c993d4276cd357691fdea61cbce738a24a
lenlen1982 <lenlen1982@makehuman.org>
2010-09-05 10:10:50 +0000

    Modified name of imported scan in scan_reg module
    git-svn-id: http://makehuman.googlecode.com/svn@1339 ea038faa-237f-11de-8bc4-bb72633a7edc

--- trunk/makehuman/utils/maketarget/maketarget.py
+++ trunk/makehuman/utils/maketarget/maketarget.py
@@ -320,7 +320,7 @@ def scanReg(scan):
     startEditing()
     (vertsM, facesM) = maketargetlib.scanRegistration(scan)
     name = scan.split('\\')[-1].split('/')[-1]
-    ob = createMesh(vertsM, facesM, "scan_reg")
+    ob = createMesh(vertsM, facesM, "scan_mesh")
     #apply rotation matrix as the base
     #matrixR = RotationMatrix(90, 4, 'x')
     #put scan on the left of base
