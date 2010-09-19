bcc1733715d30af9ab8a85a1731ebdac74206424
mbastioni <mbastioni@makehuman.org>
2010-09-19 20:03:42 +0000

    Restored gui/guiadvanced.py to don't ruin the nightly build. I'll try tomorrow.
    git-svn-id: http://makehuman.googlecode.com/svn@1378 ea038faa-237f-11de-8bc4-bb72633a7edc

---
+++ trunk/makehuman/apps/gui/guiadvanced.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Module containing classes to handle modelling mode GUI operations.
+
+**Project Name:**      MakeHuman
+
+**Product Home Page:** http://www.makehuman.org/
+
+**Code Home Page:**    http://code.google.com/p/makehuman/
+
+**Authors:**           Marc Flerackers, Jose Capco
+
+**Copyright(c):**      MakeHuman Team 2001-2010
+
+**Licensing:**         GPL3 (see also http://sites.google.com/site/makehumandocs/licensing)
+
+**Coding Standards:**  See http://sites.google.com/site/makehumandocs/developers-guide
+
+Abstract
+--------
+
+This module implements the 'guiadvance' class structures and methods to support GUI
+Advance mode operations.
+Advance mode is invoked by selecting the Advance mode icon from the main GUI control
+bar at the top of the screen.
+While in this mode, user actions (keyboard and mouse events) are passed into
+this class for processing. Having processed an event this class returns control to the
+main OpenGL/SDL/Application event handling loop.
+
+
+"""
+
+__docformat__ = 'restructuredtext'
+
+import gui3d
+
+class AdvancedCategory(gui3d.Category):
+
+    def __init__(self, category):
+        gui3d.Category.__init__(self, category, 'Advanced', category.app.getThemeResource('images', 'button_advance.png'), category.app.getThemeResource('images', 'button_advance_on.png'))
+        
+    
+
