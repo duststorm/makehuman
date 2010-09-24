c0c62fecb265f7be3b370d7036d51d5c69d15c92
mbastioni <mbastioni@makehuman.org>
2010-09-24 09:35:49 +0000

    Added undo to face asymmetry
    git-svn-id: http://makehuman.googlecode.com/svn@1403 ea038faa-237f-11de-8bc4-bb72633a7edc

--- trunk/makehuman/plugins/asymm.py
+++ trunk/makehuman/plugins/asymm.py
@@ -1,3 +1,4 @@
+import os.path
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # We need this for gui controls
@@ -9,56 +10,113 @@ import algos3d
 import mh
 
 
+
+class AsymmAction:
+
+    def __init__(self, human, targets, values):
+        """
+        Init function.
+
+        @return: None
+        @type  human: Human object
+        @param human: The makehuman human model.
+        @type  targets: List
+        @param targets: A list with the names of targets used for the asymmetry action.
+        """
+        self.name = "asymm"
+        self.human = human
+        self.before ={}
+        for t in targets:
+            try:
+                v = self.human.targetsDetailStack[t]
+            except:
+                v = 0
+            self.before[t] = v            
+        self.after = values
+        
+
+    def applyAsymm(self,asymDict):
+        """
+        This function apply all asymmetry targets, passed using a dictionary,
+        and update the human target stack.
+
+        @return: None
+        @type  asymDict: Dictonary
+        @param asymDict: A dictionary with targetpath:val items.
+        """
+        for k, v in asymDict.items():           
+            self.human.targetsDetailStack[k] = v
+        self.human.applyAllTargets(self.human.app.progress)
+
+    def do(self):
+        self.applyAsymm(self.after)
+        return True
+
+    def undo(self):
+        self.applyAsymm(self.before)
+        return True
+
+
 class AsymmTaskView(gui3d.TaskView):
 
     def __init__(self, category):
         gui3d.TaskView.__init__(self, category, 'Asymm', category.app.getThemeResource('images', 'button_asymm.png'), category.app.getThemeResource('images', 'button_asymm_on.png'))
+
+        #Sliders
         self.asymmFaceSlider = gui3d.Slider(self,  position=[20, 150, 9.3], value=0.5, label="Face asymmetry")
+
+        self.asymmDataPath = "data/targets/asym/"
+        self.asymmetryAllTargets = []
+        self.asymmetryFaceTargets = []
 
         @self.asymmFaceSlider.event
         def onChange(value):
-            self.faceAsymm(value)
-            self.applyAsymm()
+            self.app.do(AsymmAction(self.app.scene3d.selectedHuman, self.asymmetryAllTargets, self.calcAsymm(value,"face")))
 
-        self.faceAsymmDetails = {}
-        self.facePath = "data/targets/asym/face/"
 
-    def faceAsymmReset(self):
-        for k,v in self.faceAsymmDetails.items():
-            algos3d.loadTranslationTarget(self.app.scene3d.selectedHuman.meshData, k, -v, update=1, calcNorm=0)
-            self.app.scene3d.selectedHuman.targetsDetailStack[k] = 0
-            self.faceAsymmDetails[k] = 0
-        
-    def faceAsymm(self, value):
-        
-        self.faceAsymmReset()
+
+
+    def calcAsymm(self, value, bodypart):
+        """
+        This function load all asymmetryc targets for the specified part of body
+        (for example face, trunk, etc..) and return a dictionary with targetPath:val items.
+        The targets are randomly applyed, using the val coefficient.
+
+        @return: Dictonary
+        @type  value: Float
+        @param value: The amount of asymmetry
+        @type  bodypart: String
+        @param bodypart: The name of part to asymmetrize. Note it must be the same of the folder
+                        that contain the asymmetry targets.
+        """
+
+        #We use a set to overwrite double targets, in order to avoid left and right are applied in the same time.
         asymmZones = set()
-        targetsList = os.listdir(self.facePath)
+        asymmDataPartPath = os.path.join(self.asymmDataPath,bodypart)
+        targetsList = os.listdir(asymmDataPartPath)
         for t in targetsList:
-            if os.path.isfile(os.path.join(self.facePath, t)):
+            tPath = os.path.join(asymmDataPartPath, t)
+            if os.path.isfile(tPath):
+                self.asymmetryFaceTargets.append(tPath)
+                self.asymmetryAllTargets.append(tPath)
                 nameData = t.split("-")
                 asymmZones.add(nameData[1]+"-"+ nameData[2])
-        
+
+        asymmPathAndVal = {}
         for t in asymmZones:
             asymmType = random.randint(1,2) #1 = left, 2 = right
             asymmValue = random.random()* value
-            a = self.facePath+"asym-"+t+"-"+str(asymmType)+".target"
-            self.faceAsymmDetails[a] = asymmValue
+            a = os.path.join(asymmDataPartPath,"asym-"+t+"-"+str(asymmType)+".target")
+            asymmPathAndVal[a] = asymmValue
+        return asymmPathAndVal
 
-    def applyAsymm(self):
-        
-        for k,v in self.faceAsymmDetails.items():
-            algos3d.loadTranslationTarget(self.app.scene3d.selectedHuman.meshData, k, v, update=1, calcNorm=0)
-            self.app.scene3d.selectedHuman.targetsDetailStack[k] = v
-        self.app.scene3d.selectedHuman.meshData.update()
 
 def load(app):
     category = app.getCategory('Advanced','button_advance.png','button_advance_on.png')
     taskview = AsymmTaskView(category)
     print 'Asymm loaded'
 
-    #Zoom the camera
-
+    #Zoom and pan the camera
     @taskview.event
     def onMouseWheel(event):
         if event.wheelDelta > 0:
