fd0f6830817c123d3a1028c1d44ec18a64ea99d9
mflerackers <mflerackers@makehuman.org>
2011-02-01 10:11:43 +0000

    Fixed api issues
    git-svn-id: http://makehuman.googlecode.com/svn@1887 ea038faa-237f-11de-8bc4-bb72633a7edc

--- trunk/makehuman/plugins/_povray.py
+++ trunk/makehuman/plugins/_povray.py
@@ -15,44 +15,49 @@ import gui3d
 
 print 'povray imported'
 
-povray = None
+class PovrayTaskView(gui3d.TaskView):
+
+    def __init__(self, category):
+        gui3d.TaskView.__init__(self, category, 'Povray')
+
+        y = 80
+        gui3d.GroupBox(self, [10, y, 9.0], 'Options', gui3d.GroupBoxStyle._replace(height=25+24*1+6));y+=25
+        
+        #Buttons
+        self.renderButton = gui3d.Button(self, [18, y, 9.02], 'Render');y+=24
+
+        @self.renderButton.event
+        def onClicked(event):            
+            
+            reload(mh2povray)  # Avoid having to close and reopen MH for every coding change (can be removed once testing is complete)
+            for obj in self.app.scene3d.objects:
+
+            # print "POV-Ray Export test: ", obj.name
+            # Only process the humanoid figure
+
+                if obj.name == 'base.obj':
+                    mh2povray.povrayExport(obj, self.app)
+
+    def onShow(self, event):
+        self.renderButton.setFocus()
+        gui3d.TaskView.onShow(self, event)
 
 # This method is called when the plugin is loaded into makehuman
 # The app reference is passed so that a plugin can attach a new category, task, or other GUI elements
 
 
 def load(app):
-    category = app.getCategory('Rendering','button_render.png','button_render_on.png')
-    povray = gui3d.TaskView(category, 'Povray', app.getThemeResource('images', 'button_povray.png'))
+    category = app.getCategory('Rendering')
+    taskview = PovrayTaskView(category)
 
-    @povray.event
-    def onShow(event):
-        pass
-
-    @povray.event
-    def onHide(event):
-        pass
-
-    @povray.button.event
-    def onClicked(event):
-        reload(mh2povray)  # Avoid having to close and reopen MH for every coding change (can be removed once testing is complete)
-        for obj in app.scene3d.objects:
-
-      # print "POV-Ray Export test: ", obj.name
-      # Only process the humanoid figure
-
-            if obj.name == 'base.obj':
-                #mh2povray.povrayExport(obj, app.modelCamera, app.scene3d.getWindowSize())
-                mh2povray.povrayExport(obj, app)
-
-    print 'povray loaded'
-
+    print 'Povray loaded'
 
 # This method is called when the plugin is unloaded from makehuman
 # At the moment this is not used, but in the future it will remove the added GUI elements
 
 
 def unload(app):
-    print 'povray unloaded'
+    
+    print 'Povray unloaded'
 
 
