dda3e88078d661f3146677c5e623b0ae9d132cf3
mflerackers <mflerackers@makehuman.org>
2011-02-09 06:30:21 +0000

    Persistent render settings
    git-svn-id: http://makehuman.googlecode.com/svn@1961 ea038faa-237f-11de-8bc4-bb72633a7edc

--- trunk/makehuman/plugins/rendering_setting.py
+++ trunk/makehuman/plugins/rendering_setting.py
@@ -11,50 +11,73 @@ class RenderingSettingTaskView(gui3d.TaskView):
         #Rendering resolution
         y=80
         gui3d.GroupBox(self, [10, y, 9.0], 'Resolution', gui3d.GroupBoxStyle._replace(height=25+36*2+6));y+=25
-        self.widthSize= gui3d.Slider(self, position=[10, y, 9.1], value=800, min=10, max=5000, label = "Width: 800");y+=36
-        self.heightSize= gui3d.Slider(self, position=[10, y, 9.1], value=600, min=10, max=5000, label = "Height: 600");y+=36
+        rendering_width = self.app.settings.get('rendering_width', 800)
+        self.widthSize= gui3d.Slider(self, position=[10, y, 9.1], value=rendering_width, min=10, max=5000,
+            label = "Width: %d"%rendering_width);y+=36
+        rendering_height = self.app.settings.get('rendering_height', 600)
+        self.heightSize= gui3d.Slider(self, position=[10, y, 9.1], value=rendering_height, min=10, max=5000,
+            label = "Height: %d"%rendering_height);y+=36
         y+=16
+        
+        human = self.app.selectedHuman
 
         #Hair data
         gui3d.GroupBox(self, [10, y, 9.0], 'Hair', gui3d.GroupBoxStyle._replace(height=25+36*4+6));y+=25
-        self.clumpRadius = gui3d.Slider(self, position=[10, y, 9.3], value=0.09, min=0.05,max=0.5, label = "Clump radius: 0.09");y+=36
-        self.clumpChildren= gui3d.Slider(self, position=[10, y, 9.3], value=0, min=0, max=150, label = "Clump children: 0");y+=36
-        self.multiStrand= gui3d.Slider(self, position=[10, y, 9.3], value=0, min=0, max=150, label = "Multistrand children: 0");y+=36
-        self.randomHair= gui3d.Slider(self, position=[10, y, 9.3], value=0.04, min=0.0, max=0.5, label = "Randomness: 0.04");y+=36
-
-        self.human = self.app.selectedHuman
+        human.hairs.interpolationRadius = self.app.settings.get('hair.interpolationRadius',  human.hairs.interpolationRadius)
+        self.clumpRadius = gui3d.Slider(self, position=[10, y, 9.3], value=human.hairs.interpolationRadius, min=0.05,max=0.5,
+            label = "Clump radius: %.4f"%human.hairs.interpolationRadius);y+=36
+        human.hairs.clumpInterpolationNumber = self.app.settings.get('hair.clumpInterpolationNumber', human.hairs.clumpInterpolationNumber)
+        self.clumpChildren= gui3d.Slider(self, position=[10, y, 9.3], value=human.hairs.clumpInterpolationNumber, min=0, max=150,
+            label = "Clump children: %d"%human.hairs.clumpInterpolationNumber);y+=36
+        human.hairs.multiStrandNumber = self.app.settings.get('hair.multiStrandNumber',  human.hairs.multiStrandNumber)
+        self.multiStrand= gui3d.Slider(self, position=[10, y, 9.3], value=human.hairs.multiStrandNumber, min=0, max=150,
+            label = "Multistrand children: %d"%human.hairs.multiStrandNumber);y+=36
+        human.hairs.randomness = self.app.settings.get('hair.randomness',  human.hairs.randomness)
+        self.randomHair= gui3d.Slider(self, position=[10, y, 9.3], value=human.hairs.randomness, min=0.0, max=0.5,
+            label = "Randomness: %.4f"%human.hairs.randomness);y+=36
 
         @self.clumpRadius.event
         def onChanging(value):
-            self.clumpRadius.label.setText("Clump radius: "+str(round(self.clumpRadius.getValue(),4)))
-            self.human.hairs.interpolationRadius = self.clumpRadius.getValue()
+            human = self.app.selectedHuman
+            self.clumpRadius.label.setText("Clump radius: %.4f"%self.clumpRadius.getValue())
+            human.hairs.interpolationRadius = self.clumpRadius.getValue()
+            self.app.settings['hair.interpolationRadius'] = human.hairs.interpolationRadius
 
         @self.clumpChildren.event
         def onChanging(value):
-            self.clumpChildren.label.setText("Clump children: "+str(self.clumpChildren.getValue()))
-            self.human.hairs.clumpInterpolationNumber = self.clumpChildren.getValue()
+            human = self.app.selectedHuman
+            self.clumpChildren.label.setText("Clump children: %d"%self.clumpChildren.getValue())
+            human.hairs.clumpInterpolationNumber = self.clumpChildren.getValue()
+            self.app.settings['hair.clumpInterpolationNumber'] = human.hairs.clumpInterpolationNumber
             
         @self.multiStrand.event
         def onChanging(value):
-            self.multiStrand.label.setText("Multistrand children: "+str(self.multiStrand.getValue()))
-            self.human.hairs.multiStrandNumber = self.multiStrand.getValue()
+            human = self.app.selectedHuman
+            self.multiStrand.label.setText("Multistrand children: %d"%self.multiStrand.getValue())
+            human.hairs.multiStrandNumber = self.multiStrand.getValue()
+            self.app.settings['hair.multiStrandNumber'] = human.hairs.multiStrandNumber
         
         @self.randomHair.event
         def onChanging(value):
-            self.randomHair.label.setText("Randomness: "+str(round(self.randomHair.getValue(),4)))
-            self.human.hairs.randomness = self.randomHair.getValue()
-
+            human = self.app.selectedHuman
+            self.randomHair.label.setText("Randomness: %.4f"%self.randomHair.getValue())
+            human.hairs.randomness = self.randomHair.getValue()
+            self.app.settings['hair.randomness'] = human.hairs.randomness
 
         @self.widthSize.event
         def onChanging(value):
-            self.widthSize.label.setText("Width: "+str(self.widthSize.getValue()))
-            self.app.settings['rendering_width'] = self.widthSize.getValue() #Using global dictionary in app for global settings
+            self.widthSize.label.setText("Width: %d"%self.widthSize.getValue())
+            self.app.settings['rendering_width'] = self.widthSize.getValue()
 
         @self.heightSize.event
         def onChanging(value):
-            self.heightSize.label.setText("Height: "+str(self.heightSize.getValue()))
-            self.app.settings['rendering_height'] = self.heightSize.getValue() #Using global dictionary in app for global settings
+            self.heightSize.label.setText("Height: %d"%self.heightSize.getValue())
+            self.app.settings['rendering_height'] = self.heightSize.getValue()
 
+    def onHide(self, event):
+
+        gui3d.TaskView.onHide(self, event)
+        self.app.saveSettings()
 
 def load(app):
     category = app.getCategory('Rendering')
