838970687dd3baa555bb12a250afba4907713dc7
inconnu259 <inconnu259@makehuman.org>
2011-02-19 06:40:33 +0000

    Background settings
    git-svn-id: http://makehuman.googlecode.com/svn@2027 ea038faa-237f-11de-8bc4-bb72633a7edc

--- trunk/makehuman/plugins/background.py
+++ trunk/makehuman/plugins/background.py
@@ -99,6 +99,8 @@ class BackgroundTaskView(gui3d.TaskView):
 def load(app):
     category = app.getCategory('Library')
     taskview = BackgroundTaskView(category)
+    category = app.getCategory('Modelling')
+    taskview = settingsTaskView(category, taskview)
 
     print 'Background chooser loaded'
 
@@ -108,3 +110,72 @@ def load(app):
 
 def unload(app):
     print 'Hair chooser unloaded'
+    
+    
+class settingsTaskView(gui3d.TaskView) :
+    
+    def __init__(self, category, taskview):
+        
+        self.backgroundImage = taskview.backgroundImage
+        #gui3d.TaskView.__init__(self, category, 'Settings', category.app.getThemeResource('images', 'button_setting.png'), category.app.getThemeResource('images',
+        #                        'button_setting_on.png'))
+                                
+        gui3d.TaskView.__init__(self, category, 'Settings', label='Settings')
+                                
+        #gui3d.Object(self, 'data/3dobjs/unit_square.obj', self.app.getThemeResource('images', 'group_hair_tool.png'), [10, 211, 9.0], 128,256)
+        
+        #############
+        #SLIDERS
+        #############    
+        self.zoomSlider = gui3d.Slider(self, position=[10, 150, 9], value=2500, min=0.0,max=5000, label = "Zoom background")
+        self.panXSlider = gui3d.Slider(self, position=[10, 200, 9], value=250, min=0.0,max=500, label = "Pan X background")
+        self.panYSlider = gui3d.Slider(self, position=[10, 250, 9], value=250, min=0.0,max=500, label = "Pan X background")
+        
+        @self.zoomSlider.event
+        def onChanging(value):
+            self.changeZoom([value, self.zoomSlider.getValue()])
+        @self.zoomSlider.event
+        def onChange(value):
+            self.changeZoom([value, self.zoomSlider.getValue()])
+        
+        @self.panXSlider.event
+        def onChanging(value):
+            self.changePanX([value, self.panXSlider.getValue()])
+        @self.panXSlider.event
+        def onChange(value):
+            self.changePanX([value, self.panXSlider.getValue()])
+        
+        @self.panYSlider.event
+        def onChanging(value):
+            self.changePanY([value, self.panYSlider.getValue()])
+        @self.panYSlider.event
+        def onChange(value):
+            self.changePanY([value, self.panYSlider.getValue()])
+            
+    def changeZoom(self, zoom):
+        
+        #bg = self.app.categories['Library'].tasksByName['Background'].backgroundImage
+        self.backgroundImage.mesh.resize(zoom[0] , zoom[0])
+        self.app.redraw()
+        '''if bg.hasTexture():
+            bg = self.app.categories['Modelling'].tasksByName['Macro modelling'].backgroundImage
+            bg.setScale(zoom, 1.0)
+            self.app.scene3d.redraw(1)
+            print(zoom)'''
+        '''group = bg.mesh.getFaceGroup('default-dummy-group')
+            group.setColor([255, 255, 255, 100])
+            bg.setScale(zoom,1.0)
+            bg.mesh.setPickable(0)
+            self.app.scene3d.redraw(1)'''
+            
+    def changePanX(self,panX):
+        
+        #bg = self.app.categories['Library'].tasksByName['Background'].backgroundImage
+        self.backgroundImage.setPosition([panX[1], 10, 9.0])
+        self.app.redraw()
+        
+    def changePanY(self,panY):
+        
+        #bg = self.app.categories['Library'].tasksByName['Background'].backgroundImage
+        self.backgroundImage.setPosition([10, panY[1], 9.0])
+        self.app.redraw()
