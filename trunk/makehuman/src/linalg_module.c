85ff179be83ecefa26ae2b420dfe3fc12ea5d5f1
jocapsco <jocapsco@makehuman.org>
2011-02-25 06:28:18 +0000

    - some major bugfixes for mmmul.. but still dont get the right python list from mmmul
    git-svn-id: http://makehuman.googlecode.com/svn@2060 ea038faa-237f-11de-8bc4-bb72633a7edc

--- trunk/makehuman/src/linalg_module.c
+++ trunk/makehuman/src/linalg_module.c
@@ -2,57 +2,62 @@
 //#include <linalg_module.h>
 #undef min
 #undef max
+#include <stdio.h>
 #include <dgemm.h>
 #include <dgesvd.h>
 
 //includes lapack...
-static void double2PyObj(double* d, PyObject *_result, int i, int j)
+static PyObject *double2PyObj(double* d, int i, int j)
 {
     int index;
     int resultLen =  i*j;
-    _result = PyList_New(resultLen);
+    PyObject *_result = PyList_New(resultLen);
+    if (!_result)
+      return NULL;
     for (index = 0; index < resultLen; index++)
     {
       PyObject *item; 
       item = Py_BuildValue("d", d[index]);
-      PyList_SetItem(_result, index, item);
+      if (!item)
+      {
+        Py_DECREF(_result);
+        return NULL;
+      }
+      PyList_SET_ITEM(_result, index, item);
     }
 }
 
 //note that the returned double should be freed
-static double* PyObj2DoublePtr(PyObject *dP, int seqlen)
+static double* PyObj2DoublePtr(PyObject *dSeq, int seqlen)
 {
-    PyObject* dSeq;
+    //PyObject* dSeq;
     double *dC;
     //int seqlen;
     int i;
     
-    // get one argument as a sequence 
-    if(!PyArg_ParseTuple(dP, "O", &dSeq))
-        return dC;
     dSeq = PySequence_Fast(dSeq, "argument must be iterable");
     if(!dSeq)
-        return dC;
+        return NULL;
  
     // prepare data as an array of doubles 
     //*seqlen = PySequence_Fast_GET_SIZE(seq);
-    dC = malloc(seqlen*sizeof(double));
+    dC = (double*)malloc(seqlen*sizeof(double));
     if(!dC) 
     {
         Py_DECREF(dSeq);
         //return PyErr_NoMemory( );
-        return dC;
+        return NULL;
     }
 
     for(i=0; i < seqlen; i++) 
     {
         PyObject *fitem;
-        PyObject *item = PySequence_Fast_GET_ITEM(dP, i);
+        PyObject *item = PySequence_Fast_GET_ITEM(dSeq, i);
         if(!item) 
         {
             Py_DECREF(dSeq);
             free(dC);
-            return dC;
+            return NULL;
         }
         fitem = PyNumber_Float(item);
         if(!fitem) 
@@ -60,7 +65,7 @@ static double* PyObj2DoublePtr(PyObject *dP, int seqlen)
             Py_DECREF(dSeq);
             free(dC);
             PyErr_SetString(PyExc_TypeError, "all items must be numbers");
-            return dC;
+            return NULL;
         }
         dC[i] = PyFloat_AS_DOUBLE(fitem);
         Py_DECREF(fitem);
@@ -73,37 +78,64 @@ static double* PyObj2DoublePtr(PyObject *dP, int seqlen)
 //matrix multiplication:  m . n = result, m is an ixj matrix and n is a jxk matrix
 static PyObject* mh_dgemm(PyObject *self, PyObject *args)
 {
-  PyObject *_m, *_n, *_result;
+
+  PyObject *_m, *_n;
   double *m, *n, *result; 
   int i,j,k, index;
   //int resultLen = i*k;
   //double result[resultLen]; 
+  PyObject *_result = NULL;
   double alpha = 1;
   double beta = 0;
 
   if (!PyArg_ParseTuple(args, "OOiii", &_m, &_n, &i, &j, &k))
     return NULL;
-  
+
   // need to be freed in the end
   m = PyObj2DoublePtr(_m, i*j);
   if (m==NULL)
     return NULL;
-  
   // need to be freed in the end
   n = PyObj2DoublePtr(_n, j*k);
   if (n==NULL)
     return NULL;
       
   //convert to python list
-  result = malloc(i*k*sizeof(double));
-  if (!dgemm_("N", "B", &i, &k, &j, &alpha, m, &i, n, &j, &beta, result, &i))
-      double2PyObj(result, _result, i, k);
-  
-  free(result);
-  free(n);
-  free(m);
-  // return flat matrix list
-  return _result;
+  /*
+  result = (double*)malloc(i*k*sizeof(double));
+  dgemm_('N', 'N', &i, &k, &j, &alpha, m, &i, n, &j, &beta, result, &i);
+  {
+      _result = double2PyObj(result, i, k);
+       free(result);
+       free(n);
+       free(m);
+       return _result;
+  }
+  //free(result);
+  //free(n);
+  //free(m);
+
+  else
+  {
+    int i2;
+    PyObject *lst = PyList_New(4);
+    if (!lst)
+        return NULL;
+    for (i2 = 0; i2 < 4; i2++) 
+    {
+        PyObject *num = PyFloat_FromDouble(1.0);
+        if (!num) 
+        {
+            Py_DECREF(lst);
+            return NULL;
+        }
+        PyList_SET_ITEM(lst, i2, num);   // reference to num stolen
+    }
+    return lst;   
+  }
+*/
+  return double2PyObj(m, i, j);
+  //return _m;
 }
 
 //svd:  m = u*s*vt (vt is the transposed matrix of v)
@@ -126,15 +158,15 @@ static PyObject* mh_dgesvd(PyObject *self, PyObject *args)
   // do something with m
     
   //free in the end
-  u = malloc(i*i*sizeof(double));
-  s = malloc(i*j*sizeof(double));
-  vt = malloc(j*j*sizeof(double));
+  u = (double*)malloc(i*i*sizeof(double));
+  s = (double*)malloc(i*j*sizeof(double));
+  vt = (double*)malloc(j*j*sizeof(double));
 
   if (!dgesvd_("A", "A", &i, &j, m, &i, s, u, &i, vt, &j, work, &lwork, &info))
   {
-    double2PyObj(u, _u, i, i);
-    double2PyObj(vt, _vt, j, j);
-    double2PyObj(s, _s, i, j);
+    _u = double2PyObj(u, i, i);
+    _vt = double2PyObj(vt, j, j);
+    _s = double2PyObj(s, i, j);
   }
   
   free(vt); free(s); free(u); free(m);
