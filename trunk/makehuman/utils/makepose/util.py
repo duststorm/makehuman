87f35f4b70b40e0e61afb8ba0fa6cfc387287e91
jocapsco <jocapsco@makehuman.org>
2011-03-08 05:18:07 +0000

    - some fixes
    git-svn-id: http://makehuman.googlecode.com/svn@2106 ea038faa-237f-11de-8bc4-bb72633a7edc

--- trunk/makehuman/utils/makepose/util.py
+++ trunk/makehuman/utils/makepose/util.py
@@ -1,27 +1,30 @@
-import Blender
 from Blender import Object
 
 def getSelectedVertices():
     selectedVertices = []
-	ob = Object.Get("Human")
-    for i,v in enumerate(obj.verts):
+    ob = Object.Get("HumanMesh").getData()
+    for i,v in enumerate(ob.verts):
         if v.sel == 1:
             selectedVertices.append(i)
     return selectedVertices
 	
 def getVertGroups():
     vertGroups = {}
-	obj = Object.Get("Human").getData(mesh=True)
+    obj = Object.Get("HumanMesh").getData(mesh=True)
     vertGroupNames = obj.getVertGroupNames()
     for n in vertGroupNames:
         vertGroups[n] = obj.getVertsFromGroup(n)
     return vertGroups
 	
-def getSelectedGroup(verts, vertGroups)
+def getSelectedGroup(verts, vertGroups):
 	selectedGroups=[]
 	for v in verts:
 		for key in vertGroups.keys():
-			if (v in vertGroups[k]):
-				if (k not in selectedGroups): v.append(k)
+			if (v in vertGroups[key]):
+				if (key not in selectedGroups): selectedGroups.append(key)
 				break
 	return selectedGroups
+
+vertGroups = getVertGroups();
+verts = getSelectedVertices();
+print getSelectedGroup(verts, vertGroups)
