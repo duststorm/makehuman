b5cbf1b7973c1e867cdc8d083c6d0f4e9f757a2f
jocapsco <jocapsco@makehuman.org>
2011-05-12 18:19:20 +0000

    - debug update
    git-svn-id: http://makehuman.googlecode.com/svn@2384 ea038faa-237f-11de-8bc4-bb72633a7edc

--- trunk/makehuman/utils/makepose/debug.py
+++ trunk/makehuman/utils/makepose/debug.py
@@ -1,21 +1,27 @@
 import Blender
 from Blender import Mesh, Scene
 
-def drawCube(scn, bounds, name="Cube"):
-	mesh = Mesh.Primitives.Cube(bounds[0][0]-bounds[1][0])
-	center = [0.5*(bounds[0][0]+bounds[1][0]),0.5*(bounds[0][1]+bounds[1][1]),0.5*(bounds[0][2]+bounds[1][2])]
-	obj = scn.objects.new(mesh,name)
-	obj.setLocation(*center)
+def drawBox(scn, bounds, name="Cube"):
+  x = bounds[1][0]-bounds[0][0]
+  mesh = Mesh.Primitives.Cube(x)
+  verts = mesh.verts
+  for v in verts:
+    v.co[1] = v.co[1]*(bounds[1][1]-bounds[0][1])/x
+    v.co[2] = v.co[2]*(bounds[1][2]-bounds[0][2])/x
+    
+  center = [0.5*(bounds[0][0]+bounds[1][0]),0.5*(bounds[0][1]+bounds[1][1]),0.5*(bounds[0][2]+bounds[1][2])]
+  obj = scn.objects.new(mesh,name)
+  obj.setLocation(*center)
 
   
 scn = Scene.GetCurrent() #get current scene
 
-bboxj =[[1.3661949999999998, 4.5387560000000002, -1.0908855], [1.9384319999999999, 5.9782154999999992, 0.30737949999999997]]
+bboxj =[[1.3661949999999998, 4.5287560000000004, -1.1008855], [1.891527, 5.988215499999999, 0.31737949999999998]]
 
-drawCube(scn, bboxj)
+drawBox(scn, bboxj)
 
-bboxl = [[1.844622, 4.9297319999999996, -0.94940899999999995], [4.0522844999999998, 5.8845669999999997, 0.151307]]
+bboxl = [[1.891527, 4.5287560000000004, -1.1008855], [4.0522844999999998, 5.988215499999999, 0.31737949999999998]]
 
-drawCube(scn, bboxl)
+drawBox(scn, bboxl)
 
 Blender.Redraw()
