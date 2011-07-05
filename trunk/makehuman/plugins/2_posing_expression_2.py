be56a8ed511c84ec9a6e0b09babadaf4a36bf470
mbastioni <mbastioni@makehuman.org>
2011-07-05 14:43:03 +0000

    Added the platysma action, and improved a counple of existing actions.
    git-svn-id: http://makehuman.googlecode.com/svn@2599 ea038faa-237f-11de-8bc4-bb72633a7edc

--- trunk/makehuman/plugins/2_posing_expression_2.py
+++ trunk/makehuman/plugins/2_posing_expression_2.py
@@ -61,6 +61,7 @@ class ExpressionTaskView(gui3d.TaskView):
             ('eye-right', ['closure', 'droop', 'opened-up', 'slit', 'slit2', 'slit3']),
             ('mouth', ['compression', 'corner-puller', 'depression', 'depression-retraction', 'elevation', 'eversion', 'parling', 'part-later', 'protusion', 'pursing', 'retraction', 'upward-retraction']),
             ('nose', ['depression', 'left-dilatation', 'left-elevation', 'right-dilatation', 'right-elevation', 'compression']),
+            ('neck', ['platysma']),
             ]
         
         self.groupBoxes = []
