- bugfix aljabr transpose - bugfix module3d getFacesFromVerts - finished volume gradient computation
git-svn-id: http://makehuman.googlecode.com/svn@2659 ea038faa-237f-11de-8bc4-bb72633a7edc

trunk/makehuman/core/module3d.py
@@ -1590,4 +1590,4 @@ def getFacesFromVerts(vertIndices, verts):
   #F = set();
   #for index in vertIndices:
   #  F = F.union(verts[index].sharedFaces)
-  return set([f for f in verts[i].sharedFaces for i in verIndices])
+  return set([f for i in vertIndices for f in verts[i].sharedFaces ])