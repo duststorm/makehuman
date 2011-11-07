e0e711fb288a05ba631bdfb5ba34f579b2f00375
mbastioni <mbastioni@makehuman.org>
2011-11-07 18:24:54 +0000

    [Documentation] Added a new style for doc, it look more professional now.
    git-svn-id: http://makehuman.googlecode.com/svn@3011 ea038faa-237f-11de-8bc4-bb72633a7edc

--- trunk/makehuman/docs/source/conf.py
+++ trunk/makehuman/docs/source/conf.py
@@ -112,7 +112,7 @@ pygments_style = 'sphinx'
 
 # mhtheme theme
 html_theme_path = ['.']
-html_theme = 'mhtheme'
+html_theme = 'mhtheme2'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
