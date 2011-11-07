ffc397fb1aed617fc200d725048f13559a7375ac
mbastioni <mbastioni@makehuman.org>
2011-11-07 09:27:42 +0000

    [Documentation]Removed ipython extension
    git-svn-id: http://makehuman.googlecode.com/svn@3007 ea038faa-237f-11de-8bc4-bb72633a7edc

--- trunk/makehuman/docs/source/conf.py
+++ trunk/makehuman/docs/source/conf.py
@@ -19,7 +19,7 @@ import sys, os
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.append(os.path.abspath('sphinxext'))
 sys.path.append('../core')
-
+sys.path.append("C:\Python26\Lib\site-packages\matplotlib")
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -30,8 +30,7 @@ sys.path.append('../core')
 extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode',
           'matplotlib.sphinxext.mathmpl',
           'matplotlib.sphinxext.only_directives',
-          'matplotlib.sphinxext.plot_directive',
-          'matplotlib.sphinxext.ipython_directive',
+          'matplotlib.sphinxext.plot_directive',          
           'sphinx.ext.autodoc',
           'sphinx.ext.doctest',
           'inheritance_diagram',
