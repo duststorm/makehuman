1adda30862bdebb811ccfba7541670264da5c662
joel.palmius <joepal1976@makehuman.org>
2012-07-07 09:59:33 +0000

    Utility scripts for glynnc branch
    git-svn-id: http://makehuman.googlecode.com/svn@3498 ea038faa-237f-11de-8bc4-bb72633a7edc

---
+++ branches/glynnsbranch/makehuman/INSTALL_DEB_DEPENDENCIES.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo
+echo ON UBUNTU RUN "sudo ./INSTALL_DEB_DEPENDENCIES.bash"
+echo
+
+apt-get install libsdl1.2-dev freeglut3-dev freeglut3 libpng12-dev python python-opengl python-numpy libsdl1.2debian libsdl-image1.2 glew-utils python-dev
+
