1fb1e3801e9564001a686531f35a448492b13137
joel.palmius <joepal1976@makehuman.org>
2012-08-14 14:19:39 +0000

    Look in installation's data dir for skins too, not only in user home dir
    git-svn-id: http://makehuman.googlecode.com/svn@3569 ea038faa-237f-11de-8bc4-bb72633a7edc

--- trunk/makehuman/plugins/3_libraries_human_texture.py
+++ trunk/makehuman/plugins/3_libraries_human_texture.py
@@ -60,10 +60,12 @@ HumanTextureButtonStyle = gui3d.Style(**{
 class HumanTextureTaskView(gui3d.TaskView):
 
     def __init__(self, category):
+        self.systemSkins = os.path.join('data', 'skins')
+        self.userSkins = os.path.join(mh.getPath(''), 'data', 'skins')
         gui3d.TaskView.__init__(self, category, 'Human texture', label='Skin')
         if not os.path.exists(os.path.join(mh.getPath(''), 'data', 'skins')):
             os.makedirs(os.path.join(mh.getPath(''), 'data', 'skins'))
-        self.filechooser = self.addView(gui3d.FileChooser(os.path.join(mh.getPath(''), 'data', 'skins'), 'tif', 'png'))
+        self.filechooser = self.addView(gui3d.FileChooser([self.systemSkins, self.userSkins], 'tif', 'png'))
         self.update = self.filechooser.sortBox.addView(gui3d.Button('Check for updates'))
         self.mediaSync = None
         self.currentTexture = gui3d.app.categories['Modelling'].addView(gui3d.Button(style=HumanTextureButtonStyle._replace(left=800-252, top=600-36, zIndex=9.2, normal=gui3d.app.selectedHuman.getTexture())))
