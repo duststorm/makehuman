239ad6a33ab18e33c1a080d756387b85f58bd551
mflerackers <mflerackers@makehuman.org>
2012-10-17 10:38:59 +0000

    expression chooser
    git-svn-id: http://makehuman.googlecode.com/svn@3749 ea038faa-237f-11de-8bc4-bb72633a7edc

--- trunk/makehuman/plugins/2_posing_expression.py
+++ trunk/makehuman/plugins/2_posing_expression.py
@@ -6,6 +6,8 @@ import gui3d
 import humanmodifier
 import warpmodifier
 import warp
+import os
+import mh
 
 print 'Expression imported'
 
@@ -113,10 +115,98 @@ class ExpressionTaskView(gui3d.TaskView):
                 print name, value
                 file.write('expression %s %f\n' % (name, value))
 
-    def reset(self):
+    def resetExpressions(self):
+
+        human = gui3d.app.selectedHuman
 
         for name, modifier in self.modifiers.iteritems():
-            modifier.setValue(0.0)
+            modifier.setValue(human, 0.0)
+
+    def loadExpression(self, filename):
+
+        human = gui3d.app.selectedHuman
+
+        self.resetExpressions()
+
+        f = open(filename, 'r')
+
+        for data in f.readlines():
+
+            lineData = data.split()
+
+            if len(lineData) > 0 and not lineData[0] == '#':
+
+                if lineData[0] == 'expression':
+
+                    modifier = self.modifiers.get(lineData[1], None)
+                    
+                    if modifier:
+
+                        modifier.setValue(human, float(lineData[2]))
+
+class Action:
+
+    def __init__(self, human, filename, expressionTaskView, postAction=None):
+        self.name = 'Load expression'
+        self.human = human
+        self.filename = filename
+        self.expressionTaskView = expressionTaskView
+        self.postAction = postAction
+        self.before = {}
+
+        for name, modifier in self.expressionTaskView.modifiers.iteritems():
+            self.before[name] = modifier.getValue(self.human)
+
+    def do(self):
+        self.expressionTaskView.loadExpression(self.filename)
+        self.human.applyAllTargets(gui3d.app.progress, True)
+        if self.postAction:
+            self.postAction()
+        return True
+
+    def undo(self):
+        for name, value in self.before.iteritems():
+            self.expressionTaskView.modifiers[name].setValue(self.human, value)
+        self.human.applyAllTargets(gui3d.app.progress, True)
+        if self.postAction:
+            self.postAction()
+        return True
+
+class ExpressionLoadTaskView(gui3d.TaskView):
+
+    def __init__(self, category, expressionTaskView):
+
+        gui3d.TaskView.__init__(self, category, 'Expression', label='Expression')
+
+        self.expressionTaskView = expressionTaskView
+
+        self.expressionPath = os.path.join(mh.getPath(''), 'data', 'expressions')
+
+        if not os.path.exists(self.expressionPath):
+            os.makedirs(self.expressionPath)
+
+        self.filechooser = self.addView(gui3d.FileChooser(self.expressionPath, 'mhm', 'png'))
+
+        @self.filechooser.event
+        def onFileSelected(filename):
+
+            gui3d.app.do(Action(gui3d.app.selectedHuman, filename, self.expressionTaskView))
+            
+            gui3d.app.switchCategory('Modelling')
+
+    def onShow(self, event):
+
+        # When the task gets shown, set the focus to the file chooser
+        gui3d.TaskView.onShow(self, event)
+        gui3d.app.selectedHuman.hide()
+        self.filechooser.setFocus()
+
+    def onHide(self, event):
+        gui3d.app.selectedHuman.show()
+        gui3d.TaskView.onHide(self, event)
+        
+    def onResized(self, event):
+        self.filechooser.onResized(event)
 
 # This method is called when the plugin is loaded into makehuman
 # The app reference is passed so that a plugin can attach a new category, task, or other GUI elements
@@ -128,6 +218,9 @@ def load(app):
     
     app.addLoadHandler('expression', taskview.loadHandler)
     app.addSaveHandler(taskview.saveHandler)
+
+    category = app.getCategory('Library')
+    category.addView(ExpressionLoadTaskView(category, taskview))
     
     print 'Expression loaded'
 
