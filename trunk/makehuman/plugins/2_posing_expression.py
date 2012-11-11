143aa0b8c3dc455f2945f47094fdbb60fd9f600f
thomas.larsson <thomas.larsson@makehuman.org>
2012-11-11 20:21:13 +0000

    Restored expression library after alpha 7
    git-svn-id: http://makehuman.googlecode.com/svn@3814 ea038faa-237f-11de-8bc4-bb72633a7edc

--- trunk/makehuman/plugins/2_posing_expression.py
+++ trunk/makehuman/plugins/2_posing_expression.py
@@ -5,6 +5,9 @@
 import gui3d
 import humanmodifier
 import warpmodifier
+import warp
+import os
+import mh
 
 print 'Expression imported'
 
@@ -54,13 +57,12 @@ class ExpressionTaskView(gui3d.TaskView):
             # Create sliders
             for subname in subnames:
                 
-                #modifier = humanmodifier.GenderAgeModifier('data/targets/expression/units/${gender}_${age}/%s-%s.target' % (name, subname))
                 #modifier = humanmodifier.GenderAgeEthnicModifier2('data/targets/expression/units/${ethnic}/${gender}_${age}/%s-%s.target' % (name, subname))
                 modifier = warpmodifier.WarpModifier(
                     'data/targets/expression/units/${ethnic}/${gender}_${age}/%s-%s.target' % (name, subname),
                     "face",
                     "GenderAgeEthnicModifier2")
-                self.modifiers[subname] = modifier
+                self.modifiers[name + '-' + subname] = modifier
                 slider = box.addView(ExpressionSlider(subname.capitalize(), modifier))
                 self.sliders.append(slider)
             	modifier.slider = slider
@@ -91,15 +93,15 @@ class ExpressionTaskView(gui3d.TaskView):
         human = event.human
         
         for slider in self.sliders:
-            value = slider.modifier.getValue(human)
-            if value:
-                slider.modifier.setValue(human, value)
+            slider.update()
                 
     def loadHandler(self, human, values):
         
         modifier = self.modifiers.get(values[1], None)
         if modifier:
-            modifier.setValue(human, float(values[2]))
+            value = float(values[2])
+            modifier.setValue(human, value)
+            modifier.updateValue(human, value)  # Force recompilation
        
     def saveHandler(self, human, file):
         
@@ -107,6 +109,107 @@ class ExpressionTaskView(gui3d.TaskView):
             value = modifier.getValue(human)
             if value:
                 file.write('expression %s %f\n' % (name, value))
+
+    def resetExpressions(self):
+
+        human = gui3d.app.selectedHuman
+
+        for name, modifier in self.modifiers.iteritems():
+            modifier.setValue(human, 0.0)
+            #modifier.updateValue(human, 0.0)  # Force recompilation
+
+    def loadExpression(self, filename):
+
+        human = gui3d.app.selectedHuman
+
+        self.resetExpressions()
+
+        f = open(filename, 'r')
+
+        for data in f.readlines():
+
+            lineData = data.split()
+
+            if len(lineData) > 0 and not lineData[0] == '#':
+
+                if lineData[0] == 'expression':
+                    
+                    modifier = self.modifiers.get(lineData[1], None)
+                    
+                    if modifier:
+
+                        value = float(lineData[2])
+                        modifier.setValue(human, value)
+                        modifier.updateValue(human, value)  # Force recompilation
+
+class Action:
+
+    def __init__(self, human, filename, expressionTaskView, postAction=None):
+        self.name = 'Load expression'
+        self.human = human
+        self.filename = filename
+        self.expressionTaskView = expressionTaskView
+        self.postAction = postAction
+        self.before = {}
+
+        for name, modifier in self.expressionTaskView.modifiers.iteritems():
+            self.before[name] = modifier.getValue(self.human)
+
+    def do(self):
+        self.expressionTaskView.loadExpression(self.filename)
+        self.human.applyAllTargets(gui3d.app.progress, True)
+        for slider in self.expressionTaskView.sliders:
+            slider.update()
+        if self.postAction:
+            self.postAction()
+        return True
+
+    def undo(self):
+        for name, value in self.before.iteritems():
+            self.expressionTaskView.modifiers[name].setValue(self.human, value)
+        self.human.applyAllTargets(gui3d.app.progress, True)
+        for slider in self.expressionTaskView.sliders:
+            slider.update()
+        if self.postAction:
+            self.postAction()
+        return True
+
+class ExpressionLoadTaskView(gui3d.TaskView):
+
+    def __init__(self, category, expressionTaskView):
+
+        gui3d.TaskView.__init__(self, category, 'Expression', label='Expression')
+
+        self.expressionTaskView = expressionTaskView
+
+        self.globalExpressionPath = os.path.join('data', 'expressions')
+        self.expressionPath = os.path.join(mh.getPath(''), 'data', 'expressions')
+
+        if not os.path.exists(self.expressionPath):
+            os.makedirs(self.expressionPath)
+
+        self.filechooser = self.addView(gui3d.FileChooser([self.globalExpressionPath, self.expressionPath], 'mhm', 'png'))
+
+        @self.filechooser.event
+        def onFileSelected(filename):
+
+            gui3d.app.do(Action(gui3d.app.selectedHuman, filename, self.expressionTaskView))
+            
+            gui3d.app.switchCategory('Modelling')
+
+    def onShow(self, event):
+
+        # When the task gets shown, set the focus to the file chooser
+        gui3d.TaskView.onShow(self, event)
+        gui3d.app.selectedHuman.hide()
+        self.filechooser.setFocus()
+
+    def onHide(self, event):
+        gui3d.app.selectedHuman.show()
+        gui3d.TaskView.onHide(self, event)
+        
+    def onResized(self, event):
+        self.filechooser.onResized(event)
 
 # This method is called when the plugin is loaded into makehuman
 # The app reference is passed so that a plugin can attach a new category, task, or other GUI elements
@@ -116,9 +219,12 @@ def load(app):
     category = app.getCategory('Posing')
     taskview = category.addView(ExpressionTaskView(category))
     
-    #app.addLoadHandler('expression', taskview.loadHandler)
-    #app.addSaveHandler(taskview.saveHandler)
-    
+    app.addLoadHandler('expression', taskview.loadHandler)
+    app.addSaveHandler(taskview.saveHandler)
+
+    category = app.getCategory('Library')
+    category.addView(ExpressionLoadTaskView(category, taskview))
+        
     print 'Expression loaded'
 
 # This method is called when the plugin is unloaded from makehuman
