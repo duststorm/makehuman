4254d4d071a10dac091e8d7930ddd613e2858961
joel.palmius <joepal1976@makehuman.org>
2012-11-17 11:20:58 +0000

    Detect SVN revision programmatically. Call DebugDump functionality.
    git-svn-id: http://makehuman.googlecode.com/svn@3834 ea038faa-237f-11de-8bc4-bb72633a7edc

--- trunk/makehuman/makehuman.py
+++ trunk/makehuman/makehuman.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 
+
 import sys
 import os
+import str
+
+sys.path.append('core')
+
+from debugdump import DebugDump
 
 # print os.getcwd()
+
+__version__ = filter(str.isdigit, "$Revision$")
+print __version__
 
 if __name__ == '__main__':
     if len(sys.argv) >= 2:
@@ -11,12 +20,14 @@ if __name__ == '__main__':
     else:
         mfile = './main.py'
 
+    DebugDump()
+
     if sys.platform == 'win32':
-        home = os.path.expanduser('~')
         fo = open(os.path.join(home, "python_out.txt"), "w")
         sys.stdout = fo
         fe = open(os.path.join(home, "python_err.txt"), "w")
         sys.stderr = fe
+
 
     execfile(mfile)
 
