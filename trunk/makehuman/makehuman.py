d7c919797a2dec24b4c1885f0dbb7dd63def18f2
joel.palmius <joepal1976@makehuman.org>
2012-11-18 16:56:32 +0000

    Better approach for detecting SVN revision, but still not optimal
    git-svn-id: http://makehuman.googlecode.com/svn@3849 ea038faa-237f-11de-8bc4-bb72633a7edc

--- trunk/makehuman/makehuman.py
+++ trunk/makehuman/makehuman.py
@@ -9,12 +9,34 @@ import re
 sys.path.append('core')
 
 from debugdump import DebugDump
+import subprocess 
 
 # print os.getcwd()
 
+# Default fallback to use if we can't figure out SVN revision in any other
+# way: Use this file's svn revision.
 pattern = re.compile(r'[^0-9]')
-os.environ['SVNREVISION'] = pattern.sub("", "$Revision$")
-print os.environ['SVNREVISION']
+svnrev = pattern.sub("", "$Revision$")
+
+# Try getting svn revision by calling svnversion (will only work in linux) 
+# and windows where sliksvn is installed
+output = ""
+try:
+    output = subprocess.Popen(["svnversion","."], stdout=subprocess.PIPE).communicate()[0]
+    output = pattern.sub("", output)
+    svnrev = output
+except Exception as e:
+    print "Failed to get svn version number from command line: " + format(str(e))
+
+# If output is still empty at this point, the above approaches failed and
+# we need to do something else about it
+if output == "":
+    print "have to use some other approach"
+    # Some other smart way to detect svn revision
+
+# Set SVN rev in environment so it can be used elsewhere
+print "Detected SVN revision: " + svnrev    
+os.environ['SVNREVISION'] = svnrev
 
 if __name__ == '__main__':
     if len(sys.argv) >= 2:
