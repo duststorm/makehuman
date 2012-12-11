4950177e1b9626ae90df3bb7ca252f08303ed13c
joel.palmius <joepal1976@makehuman.org>
2012-12-11 10:40:13 +0000

    Updated dependencies to reflect move to QT
    git-svn-id: http://makehuman.googlecode.com/svn@3963 ea038faa-237f-11de-8bc4-bb72633a7edc

--- trunk/makehuman/INSTALL_DEB_DEPENDENCIES.bash
+++ trunk/makehuman/INSTALL_DEB_DEPENDENCIES.bash
@@ -4,5 +4,5 @@ echo
 echo ON UBUNTU RUN "sudo ./INSTALL_DEB_DEPENDENCIES.bash"
 echo
 
-apt-get install libsdl1.2-dev freeglut3-dev freeglut3 libpng12-dev python python-opengl python-numpy libsdl1.2debian libsdl-image1.2 glew-utils python-dev python-imaging
+apt-get install libsdl1.2-dev freeglut3-dev freeglut3 libpng12-dev python python-opengl python-numpy libsdl1.2debian libsdl-image1.2 glew-utils python-dev python-imaging python-opengl python-qt4 python-pyside.qtopengl python-qt4-gl python-svn
 
