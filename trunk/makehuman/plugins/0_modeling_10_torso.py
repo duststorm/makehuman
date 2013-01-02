71c62f19523ac84ffc1697c3ccbd1b8e4da0364e
mbastioni <mbastioni@makehuman.org>
2013-01-02 21:57:11 +0000

    Added stomach, pelvis and buttocks targets, using standard sliders. Now the special sliders on the right side can be deleted.
    git-svn-id: http://makehuman.googlecode.com/svn@4200 ea038faa-237f-11de-8bc4-bb72633a7edc

--- trunk/makehuman/plugins/0_modeling_10_torso.py
+++ trunk/makehuman/plugins/0_modeling_10_torso.py
@@ -10,29 +10,29 @@ import mh
 import gui
 
 class AsymmetricDetailModifier(humanmodifier.GenderAgeAsymmetricModifier):
-    
+
     def __init__(self, template, parameterName, left, right, always=True):
-    
+
         humanmodifier.GenderAgeAsymmetricModifier.__init__(self, template, parameterName, left, right, always)
-        
+
     def getValue(self, human):
-        
+
         return getattr(human, self.parameterName)
-        
+
     def setValue(self, human, value):
-        
+
         setattr(human, self.parameterName, value)
         humanmodifier.GenderAgeAsymmetricModifier.setValue(self, human, value)
 
 class StomachModifier(AsymmetricDetailModifier):
     # This needs a custom modifier because tone and weight also need to be included
-    
+
     def __init__(self):
-    
+
         AsymmetricDetailModifier.__init__(self, 'data/targets/details/${gender}-${age}-${tone}-${weight}-stomach${stomach}.target', 'stomach', '1', '2', False)
-        
+
     def expandTemplate(self, targets):
-        
+
         # Build target list of (targetname, [factors])
         targets = [(Template(target[0]).safe_substitute(gender=value), target[1] + [value]) for target in targets for value in ['female', 'male']]
         targets = [(Template(target[0]).safe_substitute(age=value), target[1] + [value]) for target in targets for value in ['child', 'young', 'old']]
@@ -43,13 +43,13 @@ class StomachModifier(AsymmetricDetailModifier):
         # Cleanup multiple hyphens and remove a possible hyphen before a dot.
         doubleHyphen = re.compile(r'-+')
         hyphenDot = re.compile(r'-\.')
-        
+
         targets = [(re.sub(hyphenDot, '.', re.sub(doubleHyphen, '-', target[0])), target[1]) for target in targets]
-        
+
         return targets
-        
+
     def getFactors(self, human, value):
-        
+
         factors = {
             'female': human.femaleVal,
             'male': human.maleVal,
@@ -65,13 +65,13 @@ class StomachModifier(AsymmetricDetailModifier):
             self.left: -min(value, 0.0),
             self.right: max(0.0, value)
         }
-        
+
         return factors
 
 class DetailSlider(humanmodifier.ModifierSlider):
-    
+
     def __init__(self, value, min, max, label, modifier):
-        
+
         humanmodifier.ModifierSlider.__init__(self, value, min, max, label, modifier=modifier)
 
 class GroupBoxRadioButton(gui.RadioButton):
@@ -79,16 +79,16 @@ class GroupBoxRadioButton(gui.RadioButton):
         super(GroupBoxRadioButton, self).__init__(group, label, selected)
         self.groupBox = groupBox
         self.task = task
-        
+
     def onClicked(self, event):
         self.task.groupBox.showWidget(self.groupBox)
-        
+
 class TorsoSlider(humanmodifier.ModifierSlider):
     def __init__(self,modifier, image, view):
         super(TorsoSlider, self).__init__(min=-1.0, max=1.0, modifier=modifier, image=image)
-        
+
         self.view = getattr(gui3d.app, view)
-        
+
     def onFocus(self, event):
         super(TorsoSlider, self).onFocus(event)
         if gui3d.app.settings.get('cameraAutoZoom', True):
@@ -98,57 +98,69 @@ class TorsoTaskView(gui3d.TaskView):
 
     def __init__(self, category):
         gui3d.TaskView.__init__(self, category, 'Torso')
-        
+
         features = [
             ('Torso', [('data/targets/torso/${ethnic}/${gender}_${age}/%s-${value}.target' % (i[0]), i[0], i[1], i[2], 'data/targets/torso/images/', i[3]) for i in
-                [                     
+                [
                     ('torso-scale-depth', 'decr', 'incr', 'setGlobalCamera'),
                     ('torso-scale-horiz', 'decr', 'incr', 'setGlobalCamera'),
                     ('torso-scale-vert', 'decr', 'incr', 'setGlobalCamera'),
                     ('torso-trans', 'in', 'out', 'setGlobalCamera'),
                     ('torso-trans', 'down', 'up', 'setGlobalCamera'),
-                    ('torso-trans', 'forward', 'backward', 'setGlobalCamera'),                          
-                                                      
-                ]]),   
+                    ('torso-trans', 'forward', 'backward', 'setGlobalCamera'),
+
+                ]]),
             ('Hip', [('data/targets/hip/${ethnic}/${gender}_${age}/%s-${value}.target' % (i[0]), i[0], i[1], i[2], 'data/targets/hip/images/', i[3]) for i in
-                [                     
+                [
                     ('hip-scale-depth', 'decr', 'incr', 'setGlobalCamera'),
                     ('hip-scale-horiz', 'decr', 'incr', 'setGlobalCamera'),
                     ('hip-scale-vert', 'decr', 'incr', 'setGlobalCamera'),
                     ('hip-trans', 'in', 'out', 'setGlobalCamera'),
                     ('hip-trans', 'down', 'up', 'setGlobalCamera'),
-                    ('hip-trans', 'forward', 'backward', 'setGlobalCamera'),                          
-                                                      
-                ]]), 
+                    ('hip-trans', 'forward', 'backward', 'setGlobalCamera'),
+
+                ]]),
+            ('Stomack', [('data/targets/stomach/${ethnic}/${gender}_${age}/%s-${value}.target' % (i[0]), i[0], i[1], i[2], 'data/targets/stomach/images/', i[3]) for i in
+                [
+                    ('stomach-tone', 'decr', 'incr', 'setGlobalCamera'),
+                ]]),
+            ('Buttocks', [('data/targets/buttocks/${ethnic}/${gender}_${age}/%s-${value}.target' % (i[0]), i[0], i[1], i[2], 'data/targets/buttocks/images/', i[3]) for i in
+                [
+                    ('buttocks-tone', 'decr', 'incr', 'setGlobalCamera'),
+                ]]),
+            ('Pelvis', [('data/targets/pelvis/${ethnic}/${gender}_${age}/%s-${value}.target' % (i[0]), i[0], i[1], i[2], 'data/targets/pelvis/images/', i[3]) for i in
+                [
+                    ('pelvis-tone', 'decr', 'incr', 'setGlobalCamera'),
+                ]]),
             ]
-        
+
         self.groupBoxes = []
         self.radioButtons = []
         self.sliders = []
-        
+
         self.modifiers = {}
-        
+
         self.categoryBox = self.addRightWidget(gui.SliderBox('Category'))
         self.groupBox = self.addLeftWidget(gui.StackedBox())
-        
+
         for name, templates in features:
-            
+
             for index, template in enumerate(templates):
-                
+
                 if index % 12 == 0:
-                    
+
                     if len(templates) <= 12:
                         title = name.capitalize()
                     else:
                         title = '%s %d' % (name.capitalize(), index / 12 + 1)
-                        
+
                     # Create box
                     box = self.groupBox.addWidget(gui.GroupBox(title))
                     self.groupBoxes.append(box)
-                    
+
                     # Create radiobutton
                     radio = self.categoryBox.addWidget(GroupBoxRadioButton(self, self.radioButtons, title, box, selected=len(self.radioButtons) == 0))
-            
+
                 # Create sliders
                 modifier = humanmodifier.GenderAgeEthnicAsymmetricModifier(template[0], 'value', template[2], template[3], False)
                 self.modifiers['%s%d' % (name, index + 1)] = modifier
@@ -161,7 +173,7 @@ class TorsoTaskView(gui3d.TaskView):
         self.specialModifiers['pelvisTone'] = AsymmetricDetailModifier('data/targets/details/${gender}-${age}-pelvis-tone${pelvisTone}.target', 'pelvisTone', '1', '2', False)
         self.specialModifiers['buttocks'] = AsymmetricDetailModifier('data/targets/details/${gender}-${age}-nates${buttocks}.target', 'buttocks', '1', '2', False)
         self.specialModifiers['stomach'] = StomachModifier()
-        
+
         slider = DetailSlider(0.0, -1.0, 1.0, "Pelvis tone", self.specialModifiers['pelvisTone']);
         self.sliders.append(slider)
         self.categoryBox.addWidget(slider);
@@ -175,23 +187,23 @@ class TorsoTaskView(gui3d.TaskView):
         self.groupBox.showWidget(self.groupBoxes[0])
         # self.hideAllBoxes()
         # self.groupBoxes[0].show()
-        
+
     def hideAllBoxes(self):
-        
+
         for box in self.groupBoxes:
-            
+
             box.hide()
-    
+
     def onShow(self, event):
 
         gui3d.TaskView.onShow(self, event)
-        
+
         if gui3d.app.settings.get('cameraAutoZoom', True):
             gui3d.app.setGlobalCamera()
-        
+
         for slider in self.sliders:
             slider.update()
-        
+
     def onHumanChanged(self, event):
 
         human = event.human
@@ -200,23 +212,23 @@ class TorsoTaskView(gui3d.TaskView):
             slider.update()
 
     def loadHandler(self, human, values):
-        
+
         if values[0] == 'torso':
             modifier = self.modifiers.get(values[1].replace("-", " "), None)
             if modifier:
                 modifier.setValue(human, float(values[2]))
-       
+
     def saveHandler(self, human, file):
-        
+
         for name, modifier in self.modifiers.iteritems():
             value = modifier.getValue(human)
             if value:
                 file.write('torso %s %f\n' % (name.replace(" ", "-"), value))
-    
+
 def load(app):
     category = app.getCategory('Modelling')
     taskview = category.addTask(TorsoTaskView(category))
-    
+
     app.addLoadHandler('torso', taskview.loadHandler)
     app.addSaveHandler(taskview.saveHandler)
 
