2b6e4e47ff2499ddd9a3ab81b0bcf9f07c98ce8d
PwnzorBot4000 <PwnzorBot4000@makehuman.org>
2013-01-07 19:05:46 +0000

    moved projection functions from 0_modeling_background.py to shared/projection.py.
    git-svn-id: http://makehuman.googlecode.com/svn@4246 ea038faa-237f-11de-8bc4-bb72633a7edc

--- trunk/makehuman/plugins/0_modeling_background.py
+++ trunk/makehuman/plugins/0_modeling_background.py
@@ -25,24 +25,16 @@ TODO
 
 __docformat__ = 'restructuredtext'
 
-import numpy as np
-
 import gui3d
 import events3d
 import mh
 import os
 from aljabr import vsub, vadd, vdot, mtransform
 from math import floor, ceil, pi, sqrt, exp
+import projection
 import gui
 import filechooser as fc
 import log
-
-def v4to3(v):
-    v = np.asarray(v)
-    return v[:3,0] / v[3:,0]
-
-def vnorm(v):
-    return v / np.sqrt(np.sum(v ** 2, axis=-1))[...,None]
 
 def pointInRect(point, rect):
 
@@ -166,63 +158,7 @@ class BackgroundTaskView(gui3d.TaskView):
             self.backgroundImage.mesh.resize(self.backgroundWidth, self.backgroundHeight)
 
             self.reference = reference
-
-    class Shader(object):
-        pass
-
-    class UvAlphaShader(Shader):
-        def __init__(self, dst, texture, uva):
-            self.dst = dst
-            self.texture = texture
-            self.size = np.array([texture.width, texture.height])
-            self.uva = uva
-
-        def shade(self, i, xy, uvw):
-            dst = self.dst._data[xy[...,1],xy[...,0]][...,:3]
-            uva = np.sum(self.uva[i][None,None,:,:] * uvw[...,[1,2,0]][:,:,:,None], axis=2)
-            ix = np.floor(uva[:,:,:2] * self.size[None,None,:]).astype(int)
-            ix = np.minimum(ix, self.size - 1)
-            ix = np.maximum(ix, 0)
-            src = self.texture._data[ix[...,1], ix[...,0]][...,:3]
-            a = uva[:,:,2]
-            return a[:,:,None] * (src.astype(float) - dst) + dst
-
-    class ColorShader(Shader):
-        def __init__(self, colors):
-            self.colors = colors
-
-        def shade(self, i, xy, uvw):
-            return np.sum(self.colors[i][None,None,:,:] * uvw[...,[1,2,0]][:,:,:,None], axis=2)
-
-    @staticmethod
-    def RasterizeTriangles(dst, coords, shader, progress = None):
-        delta = coords - coords[:,[1,2,0],:]
-        perp = np.concatenate((delta[:,:,1,None], -delta[:,:,0,None]), axis=-1)
-        dist = np.sum(perp[:,0,:] * delta[:,2,:], axis=-1)
-        perp /= dist[:,None,None]
-        base = np.sum(perp * coords, axis=-1)
-
-        cmin = np.floor(np.amin(coords, axis=1)).astype(int)
-        cmax = np.ceil( np.amax(coords, axis=1)).astype(int)
-
-        minx = cmin[:,0]
-        maxx = cmax[:,0]
-        miny = cmin[:,1]
-        maxy = cmax[:,1]
-
-        for i in xrange(len(coords)):
-            if progress is not None and i % 100 == 0:
-                progress(i, len(coords))
-
-            ixy = np.mgrid[miny[i]:maxy[i],minx[i]:maxx[i]].transpose([1,2,0])[:,:,::-1]
-            xy = ixy + 0.5
-            uvw = np.sum(perp[i,None,None,:,:] * xy[:,:,None,:], axis=-1) - base[i,None,None,:]
-            mask = np.all(uvw > 0, axis=-1)
-            col = shader.shade(i, ixy, uvw)
-            # log.debug('dst: %s', dst._data[miny[i]:maxy[i],minx[i]:maxx[i]].shape)
-            # log.debug('src: %s', col.shape)
-            dst._data[miny[i]:maxy[i],minx[i]:maxx[i],:3][mask] = col[mask]
-
+        
     def projectBackground(self):
         if not hasattr(self, "leftTop"):
             gui3d.app.prompt("Warning", "You need to load a background before you can project it.", "OK")
@@ -238,119 +174,13 @@ class BackgroundTaskView(gui3d.TaskView):
         leftTop = gui3d.app.modelCamera.convertToScreen(*self.leftTop)
         rightBottom = gui3d.app.modelCamera.convertToScreen(*self.rightBottom)
 
-        srcImg = mh.Image(self.backgroundImage.getTexture())
-        dstImg = mh.Image(gui3d.app.selectedHuman.getTexture())
-
-        dstW = dstImg.width
-        dstH = dstImg.height
-
-        ex, ey, ez = gui3d.app.modelCamera.eye
-        eye = np.matrix([ex,ey,ez,1]).T
-        fx, fy, fz = gui3d.app.modelCamera.focus
-        focus = np.matrix([fx,fy,fz,1]).T
-        transform = mesh.object3d.transform
-        eye = v4to3(transform * eye)
-        focus = v4to3(transform * focus)
-        camera = vnorm(eye - focus)
-        # log.debug('%s %s %s', eye, focus, camera)
-
-        group_mask = np.ones(len(mesh._faceGroups), dtype=bool)
-        for g in mesh._faceGroups:
-            if g.name.startswith('joint') or g.name.startswith('helper'):
-                group_mask[g.idx] = False
-        faces = np.argwhere(group_mask[mesh.group])[...,0]
-        del group_mask
-
-        # log.debug('matrix: %s', gui3d.app.modelCamera.camera.getConvertToScreenMatrix())
-
-        texco = np.asarray([0,dstH])[None,None,:] + mesh.texco[mesh.fuvs[faces]] * np.asarray([dstW,-dstH])[None,None,:]
-        matrix = np.asarray(gui3d.app.modelCamera.camera.getConvertToScreenMatrix(mesh))
-        coord = np.concatenate((mesh.coord[mesh.fvert[faces]], np.ones((len(faces),4,1))), axis=-1)
-        # log.debug('texco: %s, coord: %s', texco.shape, coord.shape)
-        coord = np.sum(matrix[None,None,:,:] * coord[:,:,None,:], axis = -1)
-        # log.debug('coord: %s', coord.shape)
-        coord = coord[:,:,:2] / coord[:,:,3:]
-        # log.debug('coord: %s', coord.shape)
-        # log.debug('coords: %f-%f, %f-%f',
-        #           np.amin(coord[...,0]), np.amax(coord[...,0]),
-        #           np.amin(coord[...,1]), np.amax(coord[...,1]))
-        # log.debug('rect: %s %s', leftTop, rightBottom)
-        coord -= np.asarray([leftTop[0], leftTop[1]])[None,None,:]
-        coord /= np.asarray([rightBottom[0] - leftTop[0], rightBottom[1] - leftTop[1]])[None,None,:]
-        alpha = np.sum(mesh.vnorm[mesh.fvert[faces]] * np.asarray(camera)[None,None,:], axis=-1)
-        alpha = np.maximum(0, alpha)
-        # alpha[...] = 1 # debug
-        # log.debug('alpha: %s', alpha.shape)
-        # log.debug('coords: %f-%f, %f-%f',
-        #           np.amin(coord[...,0]), np.amax(coord[...,0]),
-        #           np.amin(coord[...,1]), np.amax(coord[...,1]))
-        uva = np.concatenate((coord, alpha[...,None]), axis=-1)
-        # log.debug('uva: %s', uva.shape)
-        valid = np.any(alpha >= 0, axis=1)
-        # log.debug('valid: %s', valid.shape)
-        texco = texco[valid,:,:]
-        uva = uva[valid,:,:]
-
-        # log.debug('%s %s', texco.shape, uva.shape)
-
-        def progress(base, i, n):
-            gui3d.app.progress(base + 0.5 * i / n)
-
-        # log.debug('src: %s, dst: %s', srcImg._data.shape, dstImg._data.shape)
-
-        log.debug("projectBackground: begin render")
-
-        self.RasterizeTriangles(dstImg, texco[:,[0,1,2],:], self.UvAlphaShader(dstImg, srcImg, uva[:,[0,1,2],:]), progress = lambda i,n: progress(0.0,i,n))
-        self.RasterizeTriangles(dstImg, texco[:,[2,3,0],:], self.UvAlphaShader(dstImg, srcImg, uva[:,[2,3,0],:]), progress = lambda i,n: progress(0.5,i,n))
-        gui3d.app.progress(1.0)
-
-        log.debug("projectBackground: end render")
+        dstImg = projection.mapImage(mh.Image(self.backgroundImage.getTexture()))
 
         dstImg.save(os.path.join(mh.getPath(''), 'data', 'skins', 'projection.png'))
         gui3d.app.selectedHuman.setTexture(os.path.join(mh.getPath(''), 'data', 'skins', 'projection.png'))
 
     def projectLighting(self):
-
-        mesh = gui3d.app.selectedHuman.mesh
-        mesh.setShadeless(1)
-
-        dstImg = mh.Image(width=1024, height=1024, bitsPerPixel=24)
-        dstImg._data[...] = 0
-
-        dstW = dstImg.width
-        dstH = dstImg.height
-
-        delta = (-10.99, 20.0, 20.0) - mesh.coord
-        ld = vnorm(delta)
-        del delta
-        s = np.sum(ld * mesh.vnorm, axis=-1)
-        del ld
-        s = np.maximum(0, np.minimum(255, (s * 256))).astype(np.uint8)
-        mesh.color[...,:3] = s[...,None]
-        mesh.color[...,3] = 255
-        del s
-
-        group_mask = np.ones(len(mesh._faceGroups), dtype=bool)
-        for g in mesh._faceGroups:
-            if g.name.startswith('joint') or g.name.startswith('helper'):
-                group_mask[g.idx] = False
-        faces = np.argwhere(group_mask[mesh.group])[...,0]
-        del group_mask
-
-        coords = np.asarray([0,dstH])[None,None,:] + mesh.texco[mesh.fuvs[faces]] * np.asarray([dstW,-dstH])[None,None,:]
-        colors = mesh.color[mesh.fvert[faces]]
-        # log.debug("projectLighting: %s %s %s", faces.shape, coords.shape, colors.shape)
-
-        log.debug("projectLighting: begin render")
-
-        def progress(base, i, n):
-            gui3d.app.progress(base + 0.5 * i / n)
-
-        self.RasterizeTriangles(dstImg, coords[:,[0,1,2],:], self.ColorShader(colors[:,[0,1,2],:][...,:3]), progress = lambda i,n: progress(0.0,i,n))
-        self.RasterizeTriangles(dstImg, coords[:,[2,3,0],:], self.ColorShader(colors[:,[2,3,0],:][...,:3]), progress = lambda i,n: progress(0.5,i,n))
-        gui3d.app.progress(1.0)
-
-        log.debug("projectLighting: end render")
+        dstImg = projection.mapLighting()
 
         #dstImg.resize(128, 128);
 
