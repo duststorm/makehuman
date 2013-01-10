duststorm/makehuman

commit 4a910043266f1392c42d58a0e8235228977be748

Ortho camera (issue #418)
git-svn-id: http://makehuman.googlecode.com/svn@4280 ea038faa-237f-11de-8bc4-bb72633a7edc

trunk/makehuman/core/animation3d.py
@@ -271,6 +271,8 @@ class CameraAction(Action):
         self.endParams = endParams or (self.cam.eyeX, self.cam.eyeY, self.cam.eyeZ, self.cam.focusX, self.cam.focusY, self.cam.focusZ, self.cam.upX, self.cam.upY, self.cam.upZ)
 
     def set(self, alpha):
+        if self.cam.projection == 0:
+            self.cam.switchToPerspective()
         value = lerpVector(self.startParams, self.endParams, alpha)
         self.cam.eyeX, self.cam.eyeY, self.cam.eyeZ, self.cam.focusX, self.cam.focusY, self.cam.focusZ, self.cam.upX, self.cam.upY, self.cam.upZ = value
 

trunk/makehuman/core/camera3d.py
@@ -107,8 +107,6 @@ class Camera(events3d.EventHandler):
     def setEyeZ(self, value):
     
         self.camera.eyeZ = value
-        if self.camera.projection == 0:
-            self.switchToOrtho()
         self.changed()
         
     eyeZ = property(getEyeZ, setEyeZ)
@@ -146,8 +144,6 @@ class Camera(events3d.EventHandler):
     def setFocusZ(self, value):
     
         self.camera.focusZ = value
-        if self.camera.projection == 0:
-            self.switchToOrtho()
         self.changed()
         
     focusZ = property(getFocusZ, setFocusZ)
@@ -190,53 +186,18 @@ class Camera(events3d.EventHandler):
     upZ = property(getUpZ, setUpZ)
     
     @property
-    def focus(self):
+    def up(self):
         return (self.camera.upX, self.camera.upY, self.camera.upZ)
-        
-    def getLeft(self):
     
-        return self.camera.left
+    def getScale(self):
+        return self.camera.scale
 
-    def setLeft(self, value):
-    
-        self.camera.left = value
+    def setScale(self, value):
+        self.camera.scale = value
         self.changed()
-        
-    left = property(getLeft, setLeft)
-    
-    def getRight(self):
-    
-        return self.camera.right
 
-    def setRight(self, value):
-    
-        self.camera.right = value
-        self.changed()
-        
-    right = property(getRight, setRight)
-    
-    def getBottom(self):
-    
-        return self.camera.bottom
+    scale = property(getScale, setScale)
 
-    def setBottom(self, value):
-    
-        self.camera.bottom = value
-        self.changed()
-        
-    bottom = property(getBottom, setBottom)
-    
-    def getTop(self):
-    
-        return self.camera.top
-
-    def setTop(self, value):
-    
-        self.camera.top = value
-        self.changed()
-        
-    top = property(getTop, setTop)
-    
     def getStereoMode(self):
     
         return self.camera.stereoMode
@@ -270,25 +231,14 @@ class Camera(events3d.EventHandler):
         self.changedPending = False
         
     def switchToOrtho(self):
-    
-        self.camera.projection = 0
-        
-        self.camera.nearPlane = 0.001
-            
-        width, height = self.app.getWindowSize()
-        aspect = float(width) / float(height)
         fov = math.tan(self.camera.fovAngle * 0.5 * math.pi / 180.0)
-        y = aljabr.vdist(self.eye, self.focus) * fov
-        x = y * aspect
-        
-        self.camera.left = -x
-        self.camera.right = x
-        self.camera.bottom = -y
-        self.camera.top = y
-        
+        scale = aljabr.vdist(self.eye, self.focus) * fov
+
+        self.camera.projection = 0
+        self.camera.scale = scale
         self.camera.nearPlane = -100.0
         
     def switchToPerspective(self):
     
-        self.camera.projection = 0
-
+        self.camera.projection = 1
+        self.camera.nearPlane = 0.1

trunk/makehuman/core/mhmain.py
@@ -1076,7 +1076,10 @@ class MHApplication(gui3d.Application, mh.Application):
         if gui3d.app.settings.get('invertMouseWheel', False):
             speed *= -1
 
-        self.modelCamera.eyeZ -= 0.05 * event.dy * speed
+        if self.modelCamera.projection == 0:
+            self.modelCamera.scale *= 0.995 ** (event.dy * speed)
+        else:
+            self.modelCamera.eyeZ -= 0.05 * event.dy * speed
 
     def promptAndExit(self):
         if self.modified:

trunk/makehuman/lib/camera.py
@@ -53,10 +53,7 @@ class Camera(object):
         self.upX = 0.0
         self.upY = 1.0
         self.upZ = 0.0
-        self.left = 0.0
-        self.right = 0.0
-        self.bottom = 0.0
-        self.top = 0.0
+        self.scale = 1.0
 
         return self
 
@@ -74,21 +71,23 @@ class Camera(object):
         if eye:
             stereoMode = self.stereoMode
 
+        aspect = float(max(1, G.windowWidth)) / float(max(1, G.windowHeight))
+
         if stereoMode == 0:
             # No stereo
             if self.projection:
-                proj = matrix.perspective(self.fovAngle, float(max(1, G.windowWidth)) / float(max(1, G.windowHeight)), self.nearPlane, self.farPlane)
-            elif self.left == self.right and self.top == self.bottom:
-                proj = matrix.ortho(0.0, max(1, G.windowWidth), max(1, G.windowHeight), 0.0, self.nearPlane, self.farPlane)
+                proj = matrix.perspective(self.fovAngle, aspect, self.nearPlane, self.farPlane)
             else:
-                proj = matrix.ortho(self.left, self.right, self.bottom, self.top, self.nearPlane, self.farPlane)
+                height = self.scale
+                width = self.scale * aspect
+                proj = matrix.ortho(-width, width, -height, height, self.nearPlane, self.farPlane)
 
             mv = lookat(self.eyeX, self.eyeY, self.eyeZ,       # Eye
                         self.focusX, self.focusY, self.focusZ, # Focus
                         self.upX, self.upY, self.upZ)          # Up
         elif stereoMode == 1:
             # Toe-in method, uses different eye positions, same focus point and projection
-            proj = matrix.perspective(self.fovAngle, float(max(1, G.windowWidth)) / float(max(1, G.windowHeight)), self.nearPlane, self.farPlane)
+            proj = matrix.perspective(self.fovAngle, aspect, self.nearPlane, self.farPlane)
 
             if eye == 1:
                 mv = lookat(self.eyeX - 0.5 * self.eyeSeparation, self.eyeY, self.eyeZ, # Eye
@@ -100,10 +99,9 @@ class Camera(object):
                             self.upX, self.upY, self.upZ)                               # Up
         elif stereoMode == 2:
             # Off-axis method, uses different eye positions, focus points and projections
-            aspectratio = float(max(1, G.windowWidth)) / float(max(1, G.windowHeight))
             widthdiv2 = math.tan(math.radians(self.fovAngle) / 2) * self.nearPlane
-            left  = - aspectratio * widthdiv2
-            right = aspectratio * widthdiv2
+            left  = - aspect * widthdiv2
+            right = aspect * widthdiv2
             top = widthdiv2
             bottom = -widthdiv2
 

trunk/makehuman/plugins/0_modeling_background.py
@@ -114,12 +114,16 @@ class BackgroundTaskView(gui3d.TaskView):
             bg = self.backgroundImage
             bg.mesh.setTexture(os.path.join(self.backgroundsFolder, filename))
 
-            bg.setPosition([80, 80, 8])
-            bg.mesh.resize(self.texture.width, self.texture.height)
+            aspect = 1.0 * self.texture.width / self.texture.height
+            bg.setPosition([-aspect, -1, 0])
+            bg.mesh.resize(2.0 * aspect, 2.0)
             self.backgroundWidth = self.texture.width
             self.backgroundHeight = self.texture.height
             self.originalWidth = self.texture.width
             self.originalHeight = self.texture.height
+
+            # Switch to orthogonal view
+            gui3d.app.modelCamera.switchToOrtho()
 
             self.fixateBackground()
 
@@ -128,9 +132,6 @@ class BackgroundTaskView(gui3d.TaskView):
 
             mh.changeTask('Modelling', 'Background')
             gui3d.app.redraw()
-
-            # Switch to orthogonal view
-            # gui3d.app.modelCamera.switchToOrtho()
 
     def fixateBackground(self):
 

commit 74bd3b0b05ce839b669efc0020e5f426fd098a71

Fix what was broken by r4280 (including issue #418) Fix background-projection alignment (issue #412) Fix/improve other stuff related to cameras Fix background toggle
git-svn-id: http://makehuman.googlecode.com/svn@4287 ea038faa-237f-11de-8bc4-bb72633a7edc

trunk/makehuman/apps/gui/guifiles.py
@@ -35,6 +35,7 @@ from os.path import basename
 import gui
 import filechooser as fc
 import log
+import numpy as np
 
 class SaveTaskView(gui3d.TaskView):
 
@@ -48,7 +49,11 @@ class SaveTaskView(gui3d.TaskView):
         self.fileentry.setDirectory(modelPath)
         self.fileentry.setFilter('MakeHuman Models (*.mhm)')
 
-        mesh = gui3d.FrameMesh(100, 100)
+        self.selection_width = 1.2
+        self.selection_height = 1.3
+        mesh = gui3d.FrameMesh(self.selection_width, self.selection_height)
+        mesh.move(-self.selection_width/2, -self.selection_height/2)
+
         self.selection = gui3d.app.addObject(gui3d.Object([0, 0, 9], mesh))
         mesh.setColor([0, 0, 0, 255])
         mesh.setPickable(False)
@@ -72,8 +77,11 @@ class SaveTaskView(gui3d.TaskView):
 
             # Save the thumbnail
 
-            leftTop = self.selection.getPosition()
-            mh.grabScreen(int(leftTop[0]+1), int(leftTop[1]+1), int(self.selection.width-1), int(self.selection.height-1), os.path.join(dir, name + '.png'))
+            ((x0,y0,z0),(x1,y1,z1)) = self.selection.mesh.calcBBox()
+            x0,y0,z0 = gui3d.app.guiCamera.convertToScreen(x0, y0, 0)
+            x1,y1,z1 = gui3d.app.guiCamera.convertToScreen(x1, y1, 0)
+            log.debug('grab rectangle: %d %d %d %d', x0, y0, x1, y1)
+            mh.grabScreen(int(x0+1), int(y1+1), int(x1-x0-1), int(y0-y1-1), os.path.join(dir, name + '.png'))
             move(os.path.join(dir, name + '.png'), os.path.join(dir, name + '.thumb'))
 
             # Save the model
@@ -104,14 +112,6 @@ class SaveTaskView(gui3d.TaskView):
         gui3d.app.setGlobalCamera();
         gui3d.app.modelCamera.eyeZ = 70
         gui3d.app.selectedHuman.setRotation([0.0, 0.0, 0.0])
-        
-        leftTop = gui3d.app.modelCamera.convertToScreen(-10, 9, 0)
-        rightBottom = gui3d.app.modelCamera.convertToScreen(10, -10, 0)
-        
-        self.selection.setPosition([int(leftTop[0]) + 0.5, int(leftTop[1]) + 0.5, 9])
-        self.selection.width = int(rightBottom[0] - leftTop[0])
-        self.selection.height = int(rightBottom[1] - leftTop[1])
-        self.selection.mesh.resize(self.selection.width, self.selection.height)
         self.selection.show()
 
     def onHide(self, event):
@@ -128,14 +128,7 @@ class SaveTaskView(gui3d.TaskView):
         self.selection.hide()
         
     def onResized(self, event):
-    
-        leftTop = gui3d.app.modelCamera.convertToScreen(-10, 9, 0)
-        rightBottom = gui3d.app.modelCamera.convertToScreen(10, -10, 0)
-        
-        self.selection.setPosition([int(leftTop[0]) + 0.5, int(leftTop[1]) + 0.5, 9])
-        self.selection.width = int(rightBottom[0] - leftTop[0])
-        self.selection.height = int(rightBottom[1] - leftTop[1])
-        self.selection.mesh.resize(self.selection.width, self.selection.height)
+        pass
         
 class HumanFileSort(fc.FileSort):
     

trunk/makehuman/core/animation3d.py
@@ -284,7 +284,8 @@ class UpdateAction(Action):
         self.app = app
 
     def set(self, alpha):
-        self.app.redrawNow()
+        self.app.redraw()
+        self.app.processEvents()
 
 
 class Timeline:

trunk/makehuman/core/camera3d.py
@@ -210,13 +210,14 @@ class Camera(events3d.EventHandler):
     stereoMode = property(getStereoMode, setStereoMode)
         
     def convertToScreen(self, x, y, z, obj=None):
-    
         return self.camera.convertToScreen(x, y, z, obj)
         
-    def convertToWorld3D(self, x, y, z):
-    
-        return self.camera.convertToWorld3D(x, y, z)
+    def convertToWorld3D(self, x, y, z, obj=None):
+        return self.camera.convertToWorld3D(x, y, z, obj)
         
+    def convertToWorld2D(self, x, y, z, obj=None):
+        return self.camera.convertToWorld2D(x, y, z, obj)
+
     def changed(self):
         
         if self.changedPending:

trunk/makehuman/core/geometry3d.py
@@ -122,7 +122,7 @@ class RectangleMesh(module3d.Object3D):
         uv = ((0.0, 0.0), (1.0, 0.0), (1.0, 1.0), (0.0, 1.0))
         
         # The face
-        fv = [(3,2,1,0)]
+        fv = [(0,1,2,3)]
         fuv = [(0,1,2,3)]
 
         self.setCoords(v)
@@ -133,6 +133,11 @@ class RectangleMesh(module3d.Object3D):
         self.setCameraProjection(1)
         self.setShadeless(1)
         self.updateIndexBuffer()
+
+    def move(self, dx, dy):
+        self.coord += (dx, dy, 0)
+        self.markCoords(coor=True)
+        self.update()     
         
     def resize(self, width, height):
         v = [
@@ -174,7 +179,7 @@ class FrameMesh(module3d.Object3D):
         uv = ((0.0, 0.0), (1.0, 0.0), (1.0, 1.0), (0.0, 1.0))
         
         # The faces
-        f = [(3,2),(2,1),(1,0),(0,3)]
+        f = [(0,3),(1,0),(2,1),(3,2)]
 
         self.setCoords(v)
         self.setUVs(uv)
@@ -183,7 +188,12 @@ class FrameMesh(module3d.Object3D):
         self.setCameraProjection(1)
         self.setShadeless(1)
         self.updateIndexBuffer()
-        
+
+    def move(self, dx, dy):
+        self.coord += (dx, dy, 0)
+        self.markCoords(coor=True)
+        self.update()
+
     def resize(self, width, height):
         v = [
             (0.0, 0.0, 0.0),

trunk/makehuman/core/mhmain.py
@@ -959,7 +959,7 @@ class MHApplication(gui3d.Application, mh.Application):
         if not os.path.exists(grabPath):
             os.makedirs(grabPath)
         # TODO: use bbox to choose grab region
-        mh.grabScreen(180, 80, 440, 440, os.path.join(grabPath, 'grab.bmp'))
+        mh.grabScreen(0, 0, G.windowWidth, G.windowHeight, os.path.join(grabPath, 'grab.png'))
 
     # Camera navigation
     def rotateCamera(self, axis, amount):

trunk/makehuman/core/module3d.py
@@ -718,6 +718,7 @@ class Object3D(object):
         texture = getTexture(path, cache)
         
         if texture:
+            self.textureId = texture.textureId
             self.object3d.texture = texture.textureId
 
     def clearTexture(self):

trunk/makehuman/lib/camera.py
@@ -150,7 +150,7 @@ class Camera(object):
         viewport = matrix.viewport(0, 0, G.windowWidth, G.windowHeight)
         projection, modelview = self.getMatrices(0)
         m = viewport * projection * modelview
-        if obj and isinstance(obj, Object3D):
+        if obj:
             m = m * self.getObjectMatrix(obj)
         return self.getFlipMatrix() * m
 
@@ -160,33 +160,17 @@ class Camera(object):
         sx, sy, sz = matrix.transform3(m, [x, y, z])
         return [sx, sy, sz]
 
-    def convertToWorld2D(self, sx, sy):
+    def convertToWorld2D(self, sx, sy, obj = None):
         "Convert 2D (x, y) screen coordinates to OpenGL world coordinates."
 
-        viewport = matrix.viewport(0, 0, G.windowWidth, G.windowHeight)
-        projection, modelview = self.getMatrices(0)
-
-        sy = G.windowHeight - sy
-
         sz = c_double(0)
-        glReadPixels(sx, sy, 1, 1, GL_DEPTH_COMPONENT, GL_DOUBLE, byref(sz))
+        glReadPixels(sx, G.windowHeight - sy, 1, 1, GL_DEPTH_COMPONENT, GL_DOUBLE, byref(sz))
         sz = sz.value
 
-        m = viewport * projection * modelview
-        x, y, z = matrix.transform3(m.I, [sx, sy, sz])
+        return convertToWorld3D(sx, sy, sz, obj)
 
-        return [x, y, z]
-
-    def convertToWorld3D(self, sx, sy, sz):
+    def convertToWorld3D(self, sx, sy, sz, obj = None):
         "Convert 3D (x, y, depth) screen coordinates to 3D OpenGL world coordinates."
-
-        viewport = matrix.viewport(0, 0, G.windowWidth, G.windowHeight)
-        projection, modelview = self.getMatrices(0)
-
-        sy = G.windowHeight - sy
-
-        m = viewport * projection * modelview
-
+        m = self.getConvertToScreenMatrix(obj)
         x, y, z = matrix.transform3(m.I, [sx, sy, sz])
-
         return [x, y, z]

trunk/makehuman/lib/qtui.py
@@ -600,9 +600,6 @@ class Application(QtGui.QApplication, events3d.EventHandler):
     def redraw(self):
         self.mainwin.update()
         
-    def redrawNow(self):
-        gl.draw()
-        
     def getWindowSize(self):
         return G.windowWidth, G.windowHeight
 

trunk/makehuman/plugins/0_modeling_background.py
@@ -56,8 +56,8 @@ class BackgroundTaskView(gui3d.TaskView):
 
         self.filenames = {}
 
-        mesh = gui3d.RectangleMesh(420, 420)
-        self.backgroundImage = gui3d.app.categories['Modelling'].addObject(gui3d.Object([190, 90, 1], mesh, visible=False))
+        mesh = gui3d.RectangleMesh(1, 1)
+        self.backgroundImage = gui3d.app.categories['Modelling'].addObject(gui3d.Object([0, 0, 1], mesh, visible=False))
         self.opacity = 100
         mesh.setColor([255, 255, 255, self.opacity])
         mesh.setPickable(False)
@@ -94,8 +94,6 @@ class BackgroundTaskView(gui3d.TaskView):
         @self.filechooser.mhEvent
         def onFileSelected(filename):
 
-            self.reference = gui3d.app.selectedHuman.getPosition()
-
             if self.bgImageFrontRadioButton.selected:
                 self.filenames['front'] = filename
             elif self.bgImageBackRadioButton.selected:
@@ -117,63 +115,32 @@ class BackgroundTaskView(gui3d.TaskView):
             aspect = 1.0 * self.texture.width / self.texture.height
             bg.setPosition([-aspect, -1, 0])
             bg.mesh.resize(2.0 * aspect, 2.0)
-            self.backgroundWidth = self.texture.width
-            self.backgroundHeight = self.texture.height
-            self.originalWidth = self.texture.width
-            self.originalHeight = self.texture.height
 
             # Switch to orthogonal view
             gui3d.app.modelCamera.switchToOrtho()
-
-            self.fixateBackground()
 
             bg.show()
             self.backgroundImageToggle.setSelected(True)
 
             mh.changeTask('Modelling', 'Background')
             gui3d.app.redraw()
-
-    def fixateBackground(self):
-
-        self.reference = gui3d.app.selectedHuman.getPosition()
-        _, _, z = gui3d.app.modelCamera.convertToScreen(*self.reference)
-        x, y, _ = self.backgroundImage.getPosition()
-        self.leftTop = gui3d.app.modelCamera.convertToWorld3D(x, y, z)
-        self.rightBottom = gui3d.app.modelCamera.convertToWorld3D(x + self.backgroundWidth, y + self.backgroundHeight, z)
-
-    def updateBackground(self):
-
-        if self.backgroundImage.hasTexture():
-
-            reference = gui3d.app.selectedHuman.getPosition()
-            diff = vsub(reference, self.reference)
-            self.leftTop = vadd(self.leftTop, diff)
-            self.rightBottom = vadd(self.rightBottom, diff)
-
-            leftTop = gui3d.app.modelCamera.convertToScreen(*self.leftTop)
-            rightBottom = gui3d.app.modelCamera.convertToScreen(*self.rightBottom)
-
-            self.backgroundImage.setPosition([leftTop[0], leftTop[1], 8])
-            self.backgroundWidth = rightBottom[0]-leftTop[0]
-            self.backgroundHeight = rightBottom[1]-leftTop[1]
-            self.backgroundImage.mesh.resize(self.backgroundWidth, self.backgroundHeight)
-
-            self.reference = reference
         
     def projectBackground(self):
-        if not hasattr(self, "leftTop"):
+        if not self.backgroundImage.isVisible():
             gui3d.app.prompt("Warning", "You need to load a background before you can project it.", "OK")
             return
 
         mesh = gui3d.app.selectedHuman.getSeedMesh()
 
-        self.fixateBackground()
-
         # for all quads, project vertex to screen
         # if one vertex falls in bg rect, project screen quad into uv quad
         # warp image region into texture
-        leftTop = gui3d.app.modelCamera.convertToScreen(*self.leftTop)
-        rightBottom = gui3d.app.modelCamera.convertToScreen(*self.rightBottom)
+        ((x0,y0,z0), (x1,y1,z1)) = self.backgroundImage.mesh.calcBBox()
+        camera = mh.cameras[self.backgroundImage.mesh.cameraMode]
+        x0, y0, _ = camera.convertToScreen(x0, y0, z0, self.backgroundImage.mesh)
+        x1, y1, _ = camera.convertToScreen(x1, y1, z1, self.backgroundImage.mesh)
+        leftTop = (x0, y1)
+        rightBottom = (x1, y0)
 
         dstImg = projection.mapImage(mh.Image(self.backgroundImage.getTexture()), mesh, leftTop, rightBottom)
 
@@ -212,8 +179,7 @@ class BackgroundTaskView(gui3d.TaskView):
         gui3d.TaskView.onHide(self, event)
 
     def onHumanTranslated(self, event):
-
-        self.updateBackground()
+        pass
 
     def setBackgroundImage(self, side):
         filename = self.filenames.get(side)
@@ -235,14 +201,11 @@ class BackgroundTaskView(gui3d.TaskView):
         elif rot==[-90,0,0]:
             self.setBackgroundImage('bottom')
 
-        self.updateBackground()
-
     def onCameraChanged(self, event):
-
-        self.updateBackground()
+        pass
 
     def onResized(self, event):
-        self.updateBackground()
+        pass
 
 # This method is called when the plugin is loaded into makehuman
 # The app reference is passed so that a plugin can attach a new category, task, or other GUI elements
@@ -292,24 +255,37 @@ class settingsTaskView(gui3d.TaskView) :
         def onMouseDragged(event):
 
             if event.button == mh.Buttons.LEFT_MASK:
+                ((x0,y0,z0),(x1,y1,z1)) = self.backgroundImage.mesh.calcBBox()
+                x, y, z = gui3d.app.guiCamera.convertToScreen(x0, y0, z0, self.backgroundImage.mesh)
+                x += event.dx
+                y += event.dy
+                x, y, z = gui3d.app.guiCamera.convertToWorld3D(x, y, z, self.backgroundImage.mesh)
+                dx, dy = x - x0, y - y0
                 x, y, z = self.backgroundImage.getPosition()
-                self.backgroundImage.setPosition([x + event.dx, y + event.dy, z])
-                taskview.fixateBackground()
+                self.backgroundImage.setPosition([x + dx, y + dy, z])
             elif event.button == mh.Buttons.RIGHT_MASK:
+                ((x0,y0,z0),(x1,y1,z1)) = self.backgroundImage.mesh.calcBBox()
+                x0, y0, z0 = gui3d.app.guiCamera.convertToScreen(x0, y0, z0, self.backgroundImage.mesh)
+                x1, y1, z1 = gui3d.app.guiCamera.convertToScreen(x1, y1, z1, self.backgroundImage.mesh)
+                dx, dy = x1 - x0, y0 - y1
                 if abs(event.dx) > abs(event.dy):
-                    taskview.backgroundWidth += event.dx
-                    taskview.backgroundHeight = taskview.originalHeight * taskview.backgroundWidth / taskview.originalWidth
+                    dx += event.dx
+                    dy = dx * self.texture.height / self.texture.width
                 else:
-                    taskview.backgroundHeight += event.dy
-                    taskview.backgroundWidth = taskview.originalWidth * taskview.backgroundHeight / taskview.originalHeight
-                self.backgroundImage.mesh.resize(taskview.backgroundWidth, taskview.backgroundHeight)
-                taskview.fixateBackground()
+                    dy += event.dy
+                    dx = dy * self.texture.width / self.texture.height
+                x1, y0 = x0 + dx, y1 + dy
+                x0, y0, z0 = gui3d.app.guiCamera.convertToWorld3D(x0, y0, z0, self.backgroundImage.mesh)
+                x1, y1, z1 = gui3d.app.guiCamera.convertToWorld3D(x1, y1, z1, self.backgroundImage.mesh)
+                self.backgroundImage.mesh.resize(x1 - x0, y1 - y0)
+                self.backgroundImage.mesh.move(x0, y0)
 
         self.dragButton = self.backgroundBox.addWidget(gui.ToggleButton('Move && Resize'))
 
         @self.dragButton.mhEvent
         def onClicked(event):
             self.backgroundImage.mesh.setPickable(self.dragButton.selected)
+            mh.updatePickingBuffer()
 
         self.projectBackgroundButton = self.backgroundBox.addWidget(gui.Button('Project background'))
 

trunk/makehuman/plugins/5_settings_censor.py
@@ -88,7 +88,9 @@ class CensorTaskView(gui3d.TaskView):
         ]
         
         for i, v in enumerate(box):
-            box[i] = gui3d.app.modelCamera.convertToScreen(v[0], v[1], v[2], human.mesh.object3d)
+            v = gui3d.app.modelCamera.convertToScreen(v[0], v[1], v[2], human.mesh.object3d)
+            v = gui3d.app.guiCamera.convertToWorld3D(v[0], v[1], v[2])
+            box[i] = v
             
         return min([v[0] for v in box]), min([v[1] for v in box]), max([v[0] for v in box]), max([v[1] for v in box])
         