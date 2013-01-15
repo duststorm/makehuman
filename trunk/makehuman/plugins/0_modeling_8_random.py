duststorm/makehuman

commit 43efa9b6b39ebf743f0ea9ae22c9310d1d7c8ed2

Restore macro status display
git-svn-id: http://makehuman.googlecode.com/svn@4365 ea038faa-237f-11de-8bc4-bb72633a7edc

trunk/makehuman/plugins/0_modeling_modifiers.py
@@ -513,6 +513,50 @@ class MacroTaskView(ModifierTaskView):
             ]),
         ]
 
+    def syncStatus(self):
+        human = gui3d.app.selectedHuman
+        
+        if human.getGender() == 0.0:
+            gender = gui3d.app.getLanguageString('female')
+        elif human.getGender() == 1.0:
+            gender = gui3d.app.getLanguageString('male')
+        elif abs(human.getGender() - 0.5) < 0.01:
+            gender = gui3d.app.getLanguageString('neutral')
+        else:
+            gender = gui3d.app.getLanguageString('%.2f%% female, %.2f%% male') % ((1.0 - human.getGender()) * 100, human.getGender() * 100)
+        
+        if human.getAge() < 0.5:
+            age = 12 + ((25 - 12) * 2) * human.getAge()
+        else:
+            age = 25 + ((70 - 25) * 2) * (human.getAge() - 0.5)
+        
+        muscle = (human.getMuscle() * 100.0)
+        weight = (50 + (150 - 50) * human.getWeight())
+        coords = human.meshData.getCoords([8223,12361,13155])
+        height = 10 * max(coords[0][1] - coords[1][1], coords[0][1] - coords[2][1])
+        if gui3d.app.settings['units'] == 'metric':
+            units = 'cm'
+        else:
+            units = 'in'
+            height *= 0.393700787
+
+        self.setStatus('Gender: %s, Age: %d, Muscle: %.2f%%, Weight: %.2f%%, Height: %.2f %s', gender, age, muscle, weight, height, units)
+
+    def setStatus(self, format, *args):
+        gui3d.app.statusPersist(format, *args)
+
+    def onShow(self, event):
+        self.syncStatus()
+        super(MacroTaskView, self).onShow(event)
+
+    def onHide(self, event):
+        self.setStatus('')
+        super(MacroTaskView, self).onHide(event)
+
+    def onHumanChanged(self, event):
+        if self.isVisible():
+            self.syncStatus()
+
 def load(app):
     category = app.getCategory('Modelling')
 

commit 4193d296392e5d14842d9abb10a7df6ed72f45d1

Fix random plugin (issue #430) Propagate onHumanChanged() ('reset' broken by r4365)
git-svn-id: http://makehuman.googlecode.com/svn@4367 ea038faa-237f-11de-8bc4-bb72633a7edc

trunk/makehuman/plugins/0_modeling_8_random.py
@@ -23,6 +23,7 @@ TODO
 """
 
 import gui3d, random, humanmodifier
+from human import HumanEvent
 import mh
 import gui
 
@@ -97,9 +98,7 @@ class RandomTaskView(gui3d.TaskView):
                 self.storeLastRandom( 'muscle', human.getMuscle(), random.random()-0.5 )
                 
             if self.height.selected:
-                modifier = humanmodifier.Modifier('data/targets/macrodetails/universal-stature-dwarf.target',
-                    'data/targets/macrodetails/universal-stature-giant.target')
-                self.storeLastRandom( 'height', modifier.getValue(human), random.random()-0.5)
+                self.storeLastRandom( 'height', human.getHeight(), random.random()-0.5)
             
             if self.face.selected:
                 category = gui3d.app.getCategory('Modelling')
@@ -149,9 +148,7 @@ class RandomTaskView(gui3d.TaskView):
             human.setMuscle( self.getRandom('muscle', 0, 1 ))
 
         if self.height.selected:
-            modifier = humanmodifier.Modifier('data/targets/macrodetails/universal-stature-dwarf.target',
-                'data/targets/macrodetails/universal-stature-giant.target')
-            modifier.setValue(human, self.getRandom('height'), 0 ) 
+            human.setHeight( self.getRandom('height', -1, 1 ))
 
         if self.face.selected:
             category = gui3d.app.getCategory('Modelling')
@@ -182,6 +179,7 @@ class RandomTaskView(gui3d.TaskView):
                 #print "applying "+modName
                 modifiers[modName].setValue(human, self.getRandom(modName) )
 
+        human.callEvent('onChanged', HumanEvent(human, 'random'))
         human.applyAllTargets(gui3d.app.progress)
         
     # get the stored random value for the given modifierName, applying amount slider

trunk/makehuman/plugins/0_modeling_modifiers.py
@@ -109,6 +109,19 @@ class ModifierTaskView(gui3d.TaskView):
 
         self.groupBox.showWidget(self.groupBoxes[0])
 
+    def getModifiers(self):
+        return self.modifiers
+
+    def getSymmetricModifierPairNames(self):
+        return [dict(left = name, right = "l-" + name[2:])
+                for name in self.modifiers
+                if name.startswith("r-")]
+
+    def getSingularModifierNames(self):
+        return [name
+                for name in self.modifiers
+                if name[:2] not in ("r-", "l-")]
+
     def updateMacro(self):
         human = gui3d.app.selectedHuman
         for modifier in self.modifiers.itervalues():
@@ -130,7 +143,7 @@ class ModifierTaskView(gui3d.TaskView):
         for slider in self.sliders:
             slider.update()
 
-        if event.change in ('reset', 'load'):
+        if event.change in ('reset', 'load', 'random'):
             self.updateMacro()
 
     def loadHandler(self, human, values):
@@ -556,6 +569,7 @@ class MacroTaskView(ModifierTaskView):
     def onHumanChanged(self, event):
         if self.isVisible():
             self.syncStatus()
+        super(MacroTaskView, self).onHumanChanged(event)
 
 def load(app):
     category = app.getCategory('Modelling')