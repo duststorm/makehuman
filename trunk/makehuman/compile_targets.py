d17be7cb1a384b6077dfd892b388307df71e2642
duststorm <duststorm_design@hotmail.com>
2013-01-16 20:07:31 +0000

    Efficience fix for compile_targets: walk directory tree only once
    git-svn-id: http://makehuman.googlecode.com/svn@4385 ea038faa-237f-11de-8bc4-bb72633a7edc

--- trunk/makehuman/compile_targets.py
+++ trunk/makehuman/compile_targets.py
@@ -6,18 +6,28 @@ import os
 import zipfile
 import fnmatch
 
-def getFiles(rootPath, filterStr):
-    foundFiles = []
+def getAllFiles(rootPath, filterStrArr):
+    result = [ None ]*len(filterStrArr)
     for root, dirnames, filenames in os.walk(rootPath):
-        for filename in fnmatch.filter(filenames, filterStr):
-            foundFiles.append(os.path.join(root, filename))
+        for i, filterStr in enumerate(filterStrArr):
+            if not result[i]:
+                result[i] = []
+            result[i].extend(getFiles(root, filenames, filterStr))
+    return result
+
+def getFiles(root, filenames, filterStr):
+    foundFiles = []
+    for filename in fnmatch.filter(filenames, filterStr):
+        foundFiles.append(os.path.join(root, filename))
     return foundFiles
 
 
 if __name__ == '__main__':
     obj = algos3d.Target(None, None)
+    allFiles = getAllFiles('data', ['*.target', '*.png'])
     with zipfile.ZipFile('data/targets.npz', mode='w', compression=zipfile.ZIP_DEFLATED) as zip:
-        allTargets = getFiles('data', '*.target')
+        allTargets = allFiles[0]
+        print len(allFiles)
         for (i, path) in enumerate(allTargets):
             try:
                 obj._load_text(path)
@@ -30,7 +40,10 @@ if __name__ == '__main__':
             except StandardError, e:
                 print 'error converting target %s' % path
 
+    print "Writing images list"
     with open('data/images.list', 'w') as f:
-        for (i, path) in getFiles('data', '*.target'):
+        allImages = allFiles[1]
+        for path in allImages:
             path = path.replace('\\','/')
             f.write(path + '\n')
+    print "All done."
